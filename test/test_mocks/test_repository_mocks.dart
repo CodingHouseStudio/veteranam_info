e6ecd22b76f91202a4ce3c12fcdeefec9e9b2729
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-03-26 23:08:57 +0200

    add all tests

---
+++ test/test_mocks/test_repository_mocks.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+// @GenerateNiceMocks(
+//   [
+//     MockSpec<IAppAuthenticationRepository>(),
+//     MockSpec<AppAuthenticationRepository>(),
+//     MockSpec<AuthenticationRepository>(),
+//     MockSpec<IStorage>(),
+//     MockSpec<GoogleSignIn>(),
+//     MockSpec<FirebaseAuth>(),
+//     MockSpec<FirebaseModule>(),
+//     MockSpec<FirestoreService>(),
+//     MockSpec<IAuditInspectionInProgressRepository>(),
+//     MockSpec<IAuditRepository>(),
+//     MockSpec<IHomeRepository>(),
+//     MockSpec<ImageRepository>(),
+//     // MockSpec<DatePickerRepository>(),
+//     // MockSpec<BuildContext>(),
+//     MockSpec<ITemplateNewAuditNextPageRepository>(),
+//   ],
+// )
+void load() => debugPrint('loaded');
