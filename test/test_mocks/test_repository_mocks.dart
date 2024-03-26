8cfacfd6d5e6e5d6a53ae56adb2297af77686f53
Herman Kravchenko <herman.kravchenko@nicholasandco.com>
2024-03-26 11:58:20 -0600

    we may need test coverage for this

---
+++ test/test_mocks/test_repository_mocks.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+// @GenerateNiceMocks(
+//   [
+//     // MockSpec<IAppAuthenticationRepository>(),
+//     // MockSpec<AppAuthenticationRepository>(),
+//     // MockSpec<AuthenticationRepository>(),
+//     // MockSpec<IStorage>(),
+//     // MockSpec<GoogleSignIn>(),
+//     // MockSpec<FirebaseAuth>(),
+//     // MockSpec<FirebaseModule>(),
+//     // MockSpec<IAuditInspectionInProgressRepository>(),
+//     // MockSpec<IAuditRepository>(),
+//     // MockSpec<IHomeRepository>(),
+//     // MockSpec<ImageRepository>(),
+//     // MockSpec<ITemplateNewAuditNextPageRepository>(),
+//   ],
+// )
+void load() => debugPrint('loaded');
