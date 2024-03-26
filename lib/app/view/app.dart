0f8cdfd278708ece49b7b1d0432b98ad0c2bd88a
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-03-26 19:55:28 +0200

    code refactoring

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kozak/shared/navigation/navigation.dart';
+import 'package:kozak/shared/shared.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
