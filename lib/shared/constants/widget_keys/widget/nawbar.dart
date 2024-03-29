12d4ed6b9b8821fbbcc2a8ab2c9a6a87c71547a4
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-03-29 23:53:38 +0200

    merge

--- lib/shared/constants/widget_keys/widget/nawbar.dart
+++ lib/shared/constants/widget_keys/widget/nawbar.dart
@@ -6,9 +6,4 @@ class _NawbarKeys {
   final iconMic = const Key('nawbar_icon_micro');
   final button = const Key('nawbar_button');
   final iconPerson = const Key('nawbar_icon_person');
-  final title = const Key('search_title');
-  final field = const Key('search_field');
-  final micIcon = const Key('search_micro_icon');
-  final button = const Key('search_button');
-  final personIcon = const Key('search_person_icon');
 }
