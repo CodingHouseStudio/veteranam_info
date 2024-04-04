cfe3fed2bd466edab795575f1e388c319d876593
SofiiaKolpakova <sofiia.kolpakova@codinghouse.biz>
2024-04-04 16:29:13 +0300

    feat: add discount box widget on discount screen

--- lib/components/discounts/widget/body/discounts_body_widget.dart
+++ lib/components/discounts/widget/body/discounts_body_widget.dart
@@ -2,14 +2,95 @@ import 'package:flutter/material.dart';
 import 'package:kozak/shared/shared.dart';
 
 class DiscountsBodyWidget extends StatelessWidget {
-  const DiscountsBodyWidget({super.key});
+  const DiscountsBodyWidget({
+    required this.discount,
+    required this.city,
+    required this.service,
+    required this.description,
+    required this.date,
+    required this.instruction,
+    super.key,
+  });
+
+  final String discount;
+  final String city;
+  final String service;
+  final String description;
+  final String date;
+  final String instruction;
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        KSizedBox.kHeightSizedBox30,
-      ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: KPadding.kPaddingSize48,
+          vertical: KPadding.kPaddingSize16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    discount,
+                    key: KWidgetkeys.widget.discountCard.discount,
+                    style: AppTextStyle.text32,
+                    overflow: TextOverflow.clip,
+                  ),
+                ),
+                const Spacer(),
+                Expanded(
+                  child: Text(
+                    service,
+                    key: KWidgetkeys.widget.discountCard.service,
+                    style: AppTextStyle.text32,
+                    overflow: TextOverflow.clip,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              city,
+              key: KWidgetkeys.widget.discountCard.city,
+              style: AppTextStyle.text20.copyWith(
+                color: AppColors.lightGray,
+              ),
+            ),
+            Text(
+              date,
+              key: KWidgetkeys.widget.discountCard.date,
+              style: AppTextStyle.text20.copyWith(
+                color: AppColors.lightGray,
+              ),
+            ),
+            Text(
+              description,
+              key: KWidgetkeys.widget.discountCard.description,
+              style: AppTextStyle.text16,
+            ),
+            KSizedBox.kHeightSizedBox16,
+            CardTextDetailWidget(
+              text: instruction,
+              maxLines: 1,
+              icon: Row(
+                children: [
+                  Container(
+                    child: KIcon.share,
+                  ),
+                  KSizedBox.kWidthSizedBox16,
+                  Container(
+                    child: KIcon.safe,
+                  ),
+                ],
+              ),
+            ),
+            KSizedBox.kHeightSizedBox16,
+          ],
+        ),
+      ),
     );
   }
 }
