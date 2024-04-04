4fab458c772bb947d495816cb0f951fe1d6169ca
SofiiaKolpakova <sofiia.kolpakova@codinghouse.biz>
2024-04-04 12:26:47 +0300

    feat: add button for mobile add some change

--- lib/shared/widget/button_mob_widget.dart
+++ lib/shared/widget/button_mob_widget.dart
@@ -2,35 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:kozak/shared/shared.dart';
 
 class ButtonMobWidget extends StatelessWidget {
-  const ButtonMobWidget({super.key});
+  const ButtonMobWidget({required this.showGoogleIcon, super.key});
+
+  final bool showGoogleIcon;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton.icon(
-            key: KWidgetkeys.buttonMob.googleButton,
-            label: const Text(
-              KAppText.send,
-              style: AppTextStyle.lableMediumSmall,
-            ),
-            style: KButtonStyles.secondaryButtonStyle,
-            icon: KIcon.google,
-            onPressed: () {},
-          ),
-          KSizedBox.kHeightSizedBoxS,
-          ElevatedButton(
-            key: KWidgetkeys.buttonMob.button,
-            style: KButtonStyles.secondaryButtonStyle,
-            onPressed: () {},
-            child: const Text(
-              KAppText.send,
-              style: AppTextStyle.lableMediumSmall,
-            ),
-          ),
-        ],
+      child: ElevatedButton.icon(
+        key: KWidgetkeys.buttonMob.googleButton,
+        label: const Text(
+          KAppText.send,
+          style: AppTextStyle.lableMediumSmall,
+        ),
+        style: KButtonStyles.secondaryButtonStyle,
+        icon: showGoogleIcon ? KIcon.google : const SizedBox.shrink(),
+        onPressed: () {},
       ),
     );
   }
