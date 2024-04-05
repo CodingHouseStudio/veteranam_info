444da785f6feaaea23015faa461c9bf273f90ed6
SofiiaKolpakova <sofiia.kolpakova@codinghouse.biz>
2024-04-05 14:01:03 +0300

    feat: fix merge

--- lib/components/discounts/widget/body/discounts_body_widget.dart
+++ lib/components/discounts/widget/body/discounts_body_widget.dart
@@ -21,69 +21,77 @@ class DiscountsBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Text(
-                    discount,
-                    key: KWidgetkeys.widget.discountCard.discount,
-                    style: AppTextStyle.text32,
-                    overflow: TextOverflow.clip,
-                  ),
-                ),
-                const Spacer(),
-                Expanded(
-                  child: Text(
-                    service,
-                    key: KWidgetkeys.widget.discountCard.service,
-                    style: AppTextStyle.text32,
-                    overflow: TextOverflow.clip,
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              city,
-              key: KWidgetkeys.widget.discountCard.city,
-              style: AppTextStyle.text20.copyWith(
-                color: AppColors.lightGray,
-              ),
-            ),
-            Text(
-              date,
-              key: KWidgetkeys.widget.discountCard.date,
-              style: AppTextStyle.text20.copyWith(
-                color: AppColors.lightGray,
-              ),
-            ),
-            Text(
-              description,
-              key: KWidgetkeys.widget.discountCard.description,
-              style: AppTextStyle.text16,
-            ),
-            KSizedBox.kHeightSizedBox16,
-            CardTextDetailWidget(
-              text: instruction,
-              maxLines: 1,
-              icon: Row(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        decoration: KWidetTheme.boxDecorPrimary,
+        child: Padding(
+          padding: const EdgeInsets.all(KPadding.kPaddingSize10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    child: KIcon.share,
+                  Expanded(
+                    child: Text(
+                      discount,
+                      key: KWidgetkeys.widget.discountCard.discount,
+                      style: AppTextStyle.text32,
+                      overflow: TextOverflow.clip,
+                    ),
                   ),
-                  KSizedBox.kWidthSizedBox16,
-                  Container(
-                    child: KIcon.safe,
+                  const Spacer(),
+                  Expanded(
+                    child: Text(
+                      service,
+                      key: KWidgetkeys.widget.discountCard.service,
+                      style: AppTextStyle.text32,
+                      overflow: TextOverflow.clip,
+                    ),
                   ),
                 ],
               ),
-            ),
-            KSizedBox.kHeightSizedBox16,
-          ],
-        
+              Text(
+                city,
+                key: KWidgetkeys.widget.discountCard.city,
+                style: AppTextStyle.text20.copyWith(
+                  color: AppColors.lightGray,
+                ),
+              ),
+              Text(
+                date,
+                key: KWidgetkeys.widget.discountCard.date,
+                style: AppTextStyle.text20.copyWith(
+                  color: AppColors.lightGray,
+                ),
+              ),
+              Text(
+                description,
+                key: KWidgetkeys.widget.discountCard.description,
+                style: AppTextStyle.text16,
+              ),
+              KSizedBox.kHeightSizedBox16,
+              CardTextDetailWidget(
+                text: instruction,
+                maxLines: 1,
+                icon: Row(
+                  children: [
+                    Container(
+                      child: KIcon.share,
+                    ),
+                    KSizedBox.kWidthSizedBox16,
+                    Container(
+                      child: KIcon.safe,
+                    ),
+                  ],
+                ),
+              ),
+              KSizedBox.kHeightSizedBox16,
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
