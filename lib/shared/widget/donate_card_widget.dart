4800b06277c57937e060420d02bf2cecebec0890
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-04-15 08:58:40 +0300

    temporary

---
+++ lib/shared/widget/donate_card_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DonateCardWidget extends StatelessWidget {
+  const DonateCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
