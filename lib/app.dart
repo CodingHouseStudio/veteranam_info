118e7fc7f3773974ac2f0938d0ff0287ffa1e080
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-04-30 12:09:28 +0300

    change theme

--- lib/app.dart
+++ lib/app.dart
@@ -52,7 +52,7 @@ class _AppWidgetState extends State<AppWidget> {
       builder: (context, _) {
         return MaterialApp.router(
           key: KWidgetkeys.screen.app.screen,
-          theme: themeDataNew.dark(),
+          theme: themeDataNew.darkHighContrast(),
           localizationsDelegates: AppLocalizations.localizationsDelegates,
           locale: _.userSetting.locale.value,
           supportedLocales: AppLocalizations.supportedLocales,
