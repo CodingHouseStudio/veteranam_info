33f0f6073e94b51c9f9f3b900a679f5109cf4be2
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-04-30 11:33:40 +0300

    code refactoring

--- lib/components/investors/bloc/investors_watcher_event.dart
+++ lib/components/investors/bloc/investors_watcher_event.dart
@@ -2,5 +2,5 @@ part of 'investors_watcher_bloc.dart';
 
 @freezed
 class InvestorsWatcherEvent with _$InvestorsWatcherEvent {
-  const factory InvestorsWatcherEvent.started() = _Started;
+  const factory InvestorsWatcherEvent.ted() = _Started;
 }
