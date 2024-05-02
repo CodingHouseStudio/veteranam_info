bd1c62a3ac5317888443d395f29ae40312c43ef6
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-02 16:04:58 +0300

    Edited the  work employer page

--- lib/components/employer/view/employer_view.dart
+++ lib/components/employer/view/employer_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kozak/components/components.dart';
-import 'package:kozak/components/employer/widget/body/employer_body_widget.dart';
 import 'package:kozak/shared/shared.dart';
 
 class EmployerScreen extends StatelessWidget {
