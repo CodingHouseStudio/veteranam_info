d787942e1e1f017eff98c9297172a0e85cb8ec5a
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-03 12:11:28 +0300

    test information repository

--- test/information/information_unit_test.dart
+++ test/information/information_unit_test.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:kozak/components/components.dart';
 import 'package:kozak/shared/shared.dart';
 import 'package:mockito/mockito.dart';
@@ -111,12 +112,28 @@ void main() {
     });
     group('${KGroupText.repository} ', () {
       late IInformationRepository mockInformationRepository;
+      late FirestoreService mockFirestoreService;
       group('${KGroupText.successfulGet} ', () {
         setUp(() {
+          ExtendedDateTime.id = '';
+          mockFirestoreService = MockFirestoreService();
           mockInformationRepository = MockIInformationRepository();
-          when(mockInformationRepository.getInformationItems()).thenAnswer(
+          when(mockFirestoreService.getInformations()).thenAnswer(
             (_) => Stream.value(KTestText.informationModelItems),
           );
+          when(
+            mockFirestoreService.addInformation(
+              KTestText.informationModelItems.first,
+            ),
+          ).thenAnswer(
+            (realInvocation) async {},
+          );
+          if (GetIt.I.isRegistered<FirestoreService>()) {
+            GetIt.I.unregister<FirestoreService>();
+          }
+          GetIt.I.registerSingleton(mockFirestoreService);
+
+          mockInformationRepository = InformationRepository();
         });
         test('information', () async {
           expect(
@@ -124,13 +141,32 @@ void main() {
             emits(KTestText.informationModelItems),
           );
         });
+        test('mock', () async {
+          mockInformationRepository.addMockInformationItems();
+          verify(
+            mockFirestoreService.addInformation(
+              KTestText.informationModelItems.first,
+            ),
+          ).called(1);
+        });
       });
       group('${KGroupText.failureGet} ', () {
         setUp(() {
-          mockInformationRepository = MockIInformationRepository();
-          when(mockInformationRepository.getInformationItems()).thenAnswer(
-            (_) => Stream.error(KGroupText.failureGet),
+          ExtendedDateTime.id = '';
+          mockFirestoreService = MockFirestoreService();
+          //mockInformationRepository = MockIInformationRepository();
+          when(mockFirestoreService.getInformations()).thenAnswer(
+            (realInvocation) => Stream.error(
+              KGroupText.failureGet,
+            ),
           );
+
+          if (GetIt.I.isRegistered<FirestoreService>()) {
+            GetIt.I.unregister<FirestoreService>();
+          }
+          GetIt.I.registerSingleton(mockFirestoreService);
+
+          mockInformationRepository = InformationRepository();
         });
         test('information', () async {
           expect(
