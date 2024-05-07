0e4bd120dbec6ab1e594caaa57df9af769e2c437
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-05-07 19:41:09 +0300

    loading page change

--- lib/components/home/widget/body/home_body_widget.dart
+++ lib/components/home/widget/body/home_body_widget.dart
@@ -31,138 +31,96 @@ class _HomeBodyWidgetState extends State<HomeBodyWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeWatcherBloc, HomeWatcherState>(
-      builder: (context, state) {
-        switch (state) {
-          case HomeWatcherStateInitial():
-            return const CircularProgressIndicator.adaptive();
-          case HomeWatcherStateLoading():
-            return const CircularProgressIndicator.adaptive();
-          case HomeWatcherStateSuccess():
-            final questionModelItems = state.questionModelItems;
-            return BlocConsumer<FeedbackBloc, FeedbackState>(
-              listener: (context, state) {
-                if (state.formState == FeedbackEnum.clear) {
-                  nameController.clear();
-                  emailController.clear();
-                  messageController.clear();
-                }
-              },
-              buildWhen: (previous, current) =>
-                  previous.formState != current.formState ||
-                  previous.failure != current.failure,
-              builder: (context, state) {
-                return ScaffoldWidget(
-                  hasMicrophone: false,
-                  childWidgetsFunction: ({required isDesk}) => [
-                    if (isDesk)
-                      KSizedBox.kHeightSizedBox24
-                    else
-                      KSizedBox.kHeightSizedBox16,
-                    if (isDesk)
-                      Row(
+    return BlocConsumer<FeedbackBloc, FeedbackState>(
+      listener: (context, state) {
+        if (state.formState == FeedbackEnum.clear) {
+          nameController.clear();
+          emailController.clear();
+          messageController.clear();
+        }
+      },
+      buildWhen: (previous, current) =>
+          previous.formState != current.formState ||
+          previous.failure != current.failure,
+      builder: (context, state) =>
+          BlocBuilder<HomeWatcherBloc, HomeWatcherState>(
+        builder: (context, _) => ScaffoldWidget(
+          childWidgetsFunction: ({required isDesk}) {
+            final childWidgets = <Widget>[
+              if (isDesk)
+                KSizedBox.kHeightSizedBox24
+              else
+                KSizedBox.kHeightSizedBox16,
+              if (isDesk)
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: HomeBoxWidget(
+                        isDesk: isDesk,
+                      ),
+                    ),
+                    KSizedBox.kWidthSizedBox24,
+                    Expanded(
+                      child: Column(
                         children: [
-                          Expanded(
-                            flex: 2,
-                            child: HomeBoxWidget(
-                              isDesk: isDesk,
+                          BoxWidget(
+                            key: KWidgetkeys.screen.home.discountsBox,
+                            text: context.l10n.discountsCoupons,
+                            onTap: () => context.goNamedWithScroll(
+                              KRoute.discounts.name,
                             ),
+                            isDesk: isDesk,
                           ),
-                          KSizedBox.kWidthSizedBox24,
-                          Expanded(
-                            child: Column(
-                              children: [
-                                BoxWidget(
-                                  key: KWidgetkeys.screen.home.discountsBox,
-                                  text: context.l10n.discountsCoupons,
-                                  onTap: () => context.goNamedWithScroll(
-                                    KRoute.discounts.name,
-                                  ),
-                                  isDesk: isDesk,
-                                ),
-                                if (isDesk)
-                                  KSizedBox.kHeightSizedBox24
-                                else
-                                  KSizedBox.kHeightSizedBox16,
-                                BoxWidget(
-                                  key: KWidgetkeys.screen.home.workBox,
-                                  text: context.l10n.work,
-                                  onTap: () => context
-                                      .goNamedWithScroll(KRoute.work.name),
-                                  isDesk: isDesk,
-                                ),
-                              ],
-                            ),
+                          if (isDesk)
+                            KSizedBox.kHeightSizedBox24
+                          else
+                            KSizedBox.kHeightSizedBox16,
+                          BoxWidget(
+                            key: KWidgetkeys.screen.home.workBox,
+                            text: context.l10n.work,
+                            onTap: () =>
+                                context.goNamedWithScroll(KRoute.work.name),
+                            isDesk: isDesk,
                           ),
                         ],
-                      )
-                    else ...[
-                      HomeBoxWidget(
-                        isDesk: isDesk,
                       ),
-                      KSizedBox.kHeightSizedBox16,
-                      BoxWidget(
-                        key: KWidgetkeys.screen.home.discountsBox,
-                        text: context.l10n.discountsCoupons,
-                        onTap: () => context.goNamedWithScroll(
-                          KRoute.discounts.name,
-                        ),
-                        isDesk: isDesk,
-                      ),
-                      if (isDesk)
-                        KSizedBox.kHeightSizedBox24
-                      else
-                        KSizedBox.kHeightSizedBox16,
-                      BoxWidget(
-                        key: KWidgetkeys.screen.home.workBox,
-                        text: context.l10n.work,
-                        onTap: () =>
-                            context.goNamedWithScroll(KRoute.work.name),
-                        isDesk: isDesk,
-                      ),
-                    ],
-                    if (isDesk)
-                      KSizedBox.kHeightSizedBox24
-                    else
-                      KSizedBox.kHeightSizedBox16,
-                    if (isDesk)
-                      Row(
-                        children: [
-                          Expanded(
-                            child: BoxWidget(
-                              key: KWidgetkeys.screen.home.informationBox,
-                              text: context.l10n.information,
-                              onTap: () => context.goNamedWithScroll(
-                                KRoute.information.name,
-                              ),
-                              isDesk: isDesk,
-                            ),
-                          ),
-                          KSizedBox.kWidthSizedBox24,
-                          Expanded(
-                            child: BoxWidget(
-                              key: KWidgetkeys.screen.home.storyBox,
-                              text: context.l10n.stories,
-                              onTap: () =>
-                                  context.goNamedWithScroll(KRoute.story.name),
-                              isDesk: isDesk,
-                            ),
-                          ),
-                          KSizedBox.kWidthSizedBox24,
-                          Expanded(
-                            child: BoxWidget(
-                              key: KWidgetkeys.screen.home.investorsBox,
-                              text: context.l10n.investors,
-                              onTap: () => context.goNamedWithScroll(
-                                KRoute.investors.name,
-                              ),
-                              isDesk: isDesk,
-                            ),
-                          ),
-                        ],
-                      )
-                    else ...[
-                      BoxWidget(
+                    ),
+                  ],
+                )
+              else ...[
+                HomeBoxWidget(
+                  isDesk: isDesk,
+                ),
+                KSizedBox.kHeightSizedBox16,
+                BoxWidget(
+                  key: KWidgetkeys.screen.home.discountsBox,
+                  text: context.l10n.discountsCoupons,
+                  onTap: () => context.goNamedWithScroll(
+                    KRoute.discounts.name,
+                  ),
+                  isDesk: isDesk,
+                ),
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox24
+                else
+                  KSizedBox.kHeightSizedBox16,
+                BoxWidget(
+                  key: KWidgetkeys.screen.home.workBox,
+                  text: context.l10n.work,
+                  onTap: () => context.goNamedWithScroll(KRoute.work.name),
+                  isDesk: isDesk,
+                ),
+              ],
+              if (isDesk)
+                KSizedBox.kHeightSizedBox24
+              else
+                KSizedBox.kHeightSizedBox16,
+              if (isDesk)
+                Row(
+                  children: [
+                    Expanded(
+                      child: BoxWidget(
                         key: KWidgetkeys.screen.home.informationBox,
                         text: context.l10n.information,
                         onTap: () => context.goNamedWithScroll(
@@ -170,110 +128,152 @@ class _HomeBodyWidgetState extends State<HomeBodyWidget> {
                         ),
                         isDesk: isDesk,
                       ),
-                      KSizedBox.kHeightSizedBox16,
-                      BoxWidget(
+                    ),
+                    KSizedBox.kWidthSizedBox24,
+                    Expanded(
+                      child: BoxWidget(
                         key: KWidgetkeys.screen.home.storyBox,
                         text: context.l10n.stories,
                         onTap: () =>
                             context.goNamedWithScroll(KRoute.story.name),
                         isDesk: isDesk,
                       ),
-                      KSizedBox.kHeightSizedBox16,
-                      BoxWidget(
+                    ),
+                    KSizedBox.kWidthSizedBox24,
+                    Expanded(
+                      child: BoxWidget(
                         key: KWidgetkeys.screen.home.investorsBox,
                         text: context.l10n.investors,
-                        onTap: () =>
-                            context.goNamedWithScroll(KRoute.investors.name),
+                        onTap: () => context.goNamedWithScroll(
+                          KRoute.investors.name,
+                        ),
                         isDesk: isDesk,
                       ),
-                    ],
-                    if (isDesk)
-                      KSizedBox.kHeightSizedBox160
-                    else
-                      KSizedBox.kHeightSizedBox40,
-                    Padding(
-                      padding: const EdgeInsets.all(
-                        KPadding.kPaddingSize16,
-                      ),
-                      child: Text(
-                        context.l10n.faq,
-                        key: KWidgetkeys.screen.home.questionListTitle,
-                        style:
-                            isDesk ? AppTextStyle.text96 : AppTextStyle.text48,
-                      ),
                     ),
+                  ],
+                )
+              else ...[
+                BoxWidget(
+                  key: KWidgetkeys.screen.home.informationBox,
+                  text: context.l10n.information,
+                  onTap: () => context.goNamedWithScroll(
+                    KRoute.information.name,
+                  ),
+                  isDesk: isDesk,
+                ),
+                KSizedBox.kHeightSizedBox16,
+                BoxWidget(
+                  key: KWidgetkeys.screen.home.storyBox,
+                  text: context.l10n.stories,
+                  onTap: () => context.goNamedWithScroll(KRoute.story.name),
+                  isDesk: isDesk,
+                ),
+                KSizedBox.kHeightSizedBox16,
+                BoxWidget(
+                  key: KWidgetkeys.screen.home.investorsBox,
+                  text: context.l10n.investors,
+                  onTap: () => context.goNamedWithScroll(KRoute.investors.name),
+                  isDesk: isDesk,
+                ),
+              ],
+              if (isDesk)
+                KSizedBox.kHeightSizedBox160
+              else
+                KSizedBox.kHeightSizedBox40,
+              Padding(
+                padding: const EdgeInsets.all(
+                  KPadding.kPaddingSize16,
+                ),
+                child: Text(
+                  context.l10n.faq,
+                  key: KWidgetkeys.screen.home.questionListTitle,
+                  style: isDesk ? AppTextStyle.text96 : AppTextStyle.text48,
+                ),
+              ),
+            ];
+
+            switch (_) {
+              case HomeWatcherStateInitial():
+                childWidgets.add(const CircularProgressIndicator.adaptive());
+              case HomeWatcherStateLoading():
+                childWidgets.add(const CircularProgressIndicator.adaptive());
+              case HomeWatcherStateSuccess():
+                final questionModelItems = _.questionModelItems;
+
+                childWidgets.addAll([
+                  if (questionModelItems.isNotEmpty)
                     ...List.generate(
                         questionModelItems.isNotEmpty
                             ? questionModelItems.length
                             : 1, (index) {
-                      if (questionModelItems.isNotEmpty) {
-                        return Padding(
-                          padding: EdgeInsets.only(
-                            top: index != 0 ? KPadding.kPaddingSize24 : 0,
-                          ),
-                          child: QuestionWidget(
-                            questionModel: questionModelItems.elementAt(index),
-                            isDesk: isDesk,
-                          ),
-                        );
-                      } else {
-                        return TextButton(
-                          key: KWidgetkeys.screen.home.buttonMock,
-                          onPressed: () {
-                            GetIt.I.get<IHomeRepository>().addMockQuestions();
-                            context
-                                .read<HomeWatcherBloc>()
-                                .add(const HomeWatcherEvent.started());
-                          },
-                          child: Text(
-                            context.l10n.getMockData,
-                            style: AppTextStyle.text32,
-                          ),
-                        );
-                      }
-                    }),
-                    if (isDesk)
-                      KSizedBox.kHeightSizedBox160
-                    else
-                      KSizedBox.kHeightSizedBox40,
-                    SizedBox.shrink(
-                      key: feedbackKey,
-                    ),
-                    if (context.read<FeedbackBloc>().state.formState ==
-                            FeedbackEnum.success ||
-                        context.read<FeedbackBloc>().state.formState ==
-                            FeedbackEnum.sendingMessage)
-                      FeedbackBoxWidget(
-                        key: feedbackBoxKey,
-                        isDesk: isDesk,
-                        sendAgain: () => context
-                            .read<FeedbackBloc>()
-                            .add(const FeedbackEvent.sendignMessageAgain()),
-                        feedbackBoxKey: feedbackBoxKey,
-                      )
-                    else
-                      ...FeedbackWidget.feedbackWidgetList(
-                        isDesk: isDesk,
-                        context: context,
-                        nameController: nameController,
-                        emailController: emailController,
-                        messageController: messageController,
-                        feedbackKey: feedbackKey,
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          top: index != 0 ? KPadding.kPaddingSize24 : 0,
+                        ),
+                        child: QuestionWidget(
+                          questionModel: questionModelItems.elementAt(index),
+                          isDesk: isDesk,
+                        ),
+                      );
+                    })
+                  else
+                    TextButton(
+                      key: KWidgetkeys.screen.home.buttonMock,
+                      onPressed: () {
+                        GetIt.I.get<IHomeRepository>().addMockQuestions();
+                        context
+                            .read<HomeWatcherBloc>()
+                            .add(const HomeWatcherEvent.started());
+                      },
+                      child: Text(
+                        context.l10n.getMockData,
+                        style: AppTextStyle.text32,
                       ),
-                    if (isDesk)
-                      KSizedBox.kHeightSizedBox160
-                    else
-                      KSizedBox.kHeightSizedBox10,
-                  ],
-                );
-              },
-            );
+                    ),
+                ]);
 
-          case HomeWatcherStateFailure():
-          default:
-            return const CircularProgressIndicator.adaptive();
-        }
-      },
+              case HomeWatcherStateFailure():
+                childWidgets.add(const CircularProgressIndicator.adaptive());
+            }
+
+            return childWidgets
+              ..addAll([
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox160
+                else
+                  KSizedBox.kHeightSizedBox40,
+                SizedBox.shrink(
+                  key: feedbackKey,
+                ),
+                if (context.read<FeedbackBloc>().state.formState ==
+                        FeedbackEnum.success ||
+                    context.read<FeedbackBloc>().state.formState ==
+                        FeedbackEnum.sendingMessage)
+                  FeedbackBoxWidget(
+                    key: feedbackBoxKey,
+                    isDesk: isDesk,
+                    sendAgain: () => context
+                        .read<FeedbackBloc>()
+                        .add(const FeedbackEvent.sendignMessageAgain()),
+                    feedbackBoxKey: feedbackBoxKey,
+                  )
+                else
+                  ...FeedbackWidget.feedbackWidgetList(
+                    isDesk: isDesk,
+                    context: context,
+                    nameController: nameController,
+                    emailController: emailController,
+                    messageController: messageController,
+                    feedbackKey: feedbackKey,
+                  ),
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox160
+                else
+                  KSizedBox.kHeightSizedBox10,
+              ]);
+          },
+        ),
+      ),
     );
   }
 
