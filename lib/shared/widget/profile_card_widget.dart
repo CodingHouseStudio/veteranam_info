5508b85659925dd47e652be771b8bbcd73258f18
SofiiaKolpakova <sofiia.kolpakova@codinghouse.biz>
2024-05-09 14:46:30 +0300

    fix: fix edit icon and bug with buttons

--- lib/shared/widget/profile_card_widget.dart
+++ lib/shared/widget/profile_card_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kozak/shared/shared.dart';
 import 'package:kozak/shared/widget/dialogs_widget.dart';
 
