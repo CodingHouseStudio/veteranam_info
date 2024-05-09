6cdfc451e29df5d38d0be3018911539313216d60
miq80 <mikoladobriy@gmail.com>
2024-05-09 11:28:35 +0300

    key fixed and colors variable

--- lib/shared/constants/widget_keys/screen/work_respond.dart
+++ lib/shared/constants/widget_keys/screen/work_respond.dart
@@ -5,10 +5,10 @@ class _WorkRespondKeys {
   final title = const Key('work_respond_screen_title');
   final subtitle = const Key('work_respond_screen_subtitle');
   final username = const Key('work_respond_screen_username');
-  final emailField = const Key('work_respond_screen_email');
-  final emailHint = const Key('work_respond_screen_email_hint');
-  final phoneNumberField = const Key('work_respond_screen_phone_number');
-  final phoneNumberHint = const Key('work_respond_screen_phone_number_hint');
+  final emailText = const Key('work_respond_screen_email');
+  final emailField = const Key('work_respond_screen_email_hint');
+  final phoneNumberText = const Key('work_respond_screen_phone_number');
+  final phoneNumberField = const Key('work_respond_screen_phone_number_hint');
   final resume = const Key('work_respond_screen_resume');
   final upload = const Key('work_respond_screen_upload');
   final checkPoint = const Key('work_respond_screen_check_point');
