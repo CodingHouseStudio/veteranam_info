cfb5477035dd779bccc10dcc9c53c46a507cd4bb
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-13 15:56:21 +0300

    Update drop list field and tests

---
+++ test/work_employer/work_employer_widget_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+
+import '../text_dependency.dart';
+import 'helper/helper.dart';
+
+void main() {
+  setUp(configureDependenciesTest);
+
+  setUpAll(setUpGlobal);
+
+  setupFirebaseAuthMocks();
+
+  tearDown(GetIt.I.reset);
+  group('${KScreenBlocName.employer} ', () {
+    testWidgets('${KGroupText.intial} ', (tester) async {
+      await employerPumpAppHelper(tester: tester);
+
+      await employerInitialHelper(tester);
+    });
+    group('${KGroupText.goRouter} ', () {
+      late MockGoRouter mockGoRouter;
+      setUp(() => mockGoRouter = MockGoRouter());
+      testWidgets('${KGroupText.intial} ', (tester) async {
+        await employerPumpAppHelper(tester: tester, mockGoRouter: mockGoRouter);
+
+        await employerInitialHelper(tester);
+      });
+      // group('${KGroupText.goTo} ', () {
+      // });
+    });
+  });
+}
