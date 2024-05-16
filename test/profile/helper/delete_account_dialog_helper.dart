24f9a8b134b0142ef75711e611f5df6a894d7d7b
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-16 15:20:11 +0300

    update tests

---
+++ test/profile/helper/delete_account_dialog_helper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kozak/shared/constants/constants.dart';
+
+import '../../text_dependency.dart';
+
+Future<void> deleteAccountDialoglHelper(
+  WidgetTester tester,
+) async {
+  expect(
+    find.byKey(KWidgetkeys.screen.profile.screen),
+    findsOneWidget,
+  );
+
+  await tester.pumpAndSettle();
+
+  await profileCardDeleteAccountHelper(tester);
+}
