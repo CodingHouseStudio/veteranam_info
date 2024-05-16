f3adb064f92df34653ce0418b7bae58c60c602c5
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-16 15:20:11 +0300

    update tests

---
+++ test/profile/helper/delete_account_unconfirm_button_helper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kozak/shared/constants/constants.dart';
+
+import '../../text_dependency.dart';
+
+Future<void> deleteAccountUnconfirmButtonlHelper(
+  WidgetTester tester,
+  MockGoRouter mockGoRouter,
+) async {
+  expect(
+    find.byKey(KWidgetkeys.screen.profile.screen),
+    findsOneWidget,
+  );
+
+  await tester.pumpAndSettle();
+
+  await profileCardDeleteAccountHelper(tester);
+
+  await dialogUnconfirmHelper(
+    tester: tester,
+    mockGoRouter: mockGoRouter,
+  );
+}
--- test/profile/profile_widget_test.dart
+++ test/profile/profile_widget_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
-
+import 'package:kozak/components/components.dart';
+import 'package:kozak/shared/shared.dart';
 import '../text_dependency.dart';
+
 import 'helper/helper.dart';
 
 void main() {
@@ -14,19 +16,73 @@ void main() {
   tearDown(GetIt.I.reset);
   group('${KScreenBlocName.profile} ', () {
     testWidgets('${KGroupText.intial} ', (tester) async {
-      await profilePumpAppHelper(tester: tester);
+      await tester.pumpApp(const ProfileScreen());
+      expect(
+        find.byKey(KWidgetkeys.screen.profile.screen),
+        findsOneWidget,
+      );
+
+      await tester.pumpAndSettle();
     });
+    testWidgets('Show log out dialog', (tester) async {
+      await tester.pumpApp(const ProfileScreen());
+
+      await logOutDialoglHelper(tester);
+    });
+
+    testWidgets('Show delete account dialog', (tester) async {
+      await tester.pumpApp(const ProfileScreen());
+
+      await deleteAccountDialoglHelper(tester);
+    });
+
     group('${KGroupText.goRouter} ', () {
       late MockGoRouter mockGoRouter;
       setUp(() => mockGoRouter = MockGoRouter());
       testWidgets('${KGroupText.intial} ', (tester) async {
-        await profilePumpAppHelper(
-          tester: tester,
-          mockGoRouter: mockGoRouter,
-        );
+        await tester.pumpApp(const ProfileScreen(), mockGoRouter: mockGoRouter);
+
+        await profileInitialHelper(tester);
       });
-      // group('${KGroupText.goTo} ', () {
-      // });
+
+      group('${KGroupText.goTo} ', () {
+        testWidgets('Log out dialog unconfirm button pop', (tester) async {
+          await tester.pumpApp(
+            const ProfileScreen(),
+            mockGoRouter: mockGoRouter,
+          );
+
+          await logOutUnconfirmButtonlHelper(tester, mockGoRouter);
+        });
+
+        testWidgets('Log out dialog confirm button pop', (tester) async {
+          await tester.pumpApp(
+            const ProfileScreen(),
+            mockGoRouter: mockGoRouter,
+          );
+
+          await logOutConfirmButtonlHelper(tester, mockGoRouter);
+        });
+
+        testWidgets('Delete account dialog unconfirm button pop',
+            (tester) async {
+          await tester.pumpApp(
+            const ProfileScreen(),
+            mockGoRouter: mockGoRouter,
+          );
+
+          await deleteAccountUnconfirmButtonlHelper(tester, mockGoRouter);
+        });
+
+        testWidgets('Delete account dialog confirm button pop', (tester) async {
+          await tester.pumpApp(
+            const ProfileScreen(),
+            mockGoRouter: mockGoRouter,
+          );
+
+          await deleteAccountConfirmButtonlHelper(tester, mockGoRouter);
+        });
+      });
     });
   });
 }
