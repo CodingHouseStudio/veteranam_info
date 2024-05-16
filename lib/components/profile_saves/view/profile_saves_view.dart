3e1849c084b4fda92563c7027386d617214e03f0
miq80 <mikoladobriy@gmail.com>
2024-05-16 11:30:44 +0300

    profile my saves page

---
+++ lib/components/profile_saves/view/profile_saves_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:kozak/components/profile_saves/widget/body/profile_saves_body.dart';
+
+class ProfileSavesScreen extends StatelessWidget {
+  const ProfileSavesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfileSavesBody(
+      key: Key(''),
+    );
+  }
+}
---
+++ lib/components/profile_saves/widget/body/profile_saves_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:kozak/shared/shared.dart';
+
+class ProfileSavesBody extends StatelessWidget {
+  const ProfileSavesBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldWidget(
+      mainChildWidgetsFunction: ({required isDesk}) => [],
+    );
+  }
+}
