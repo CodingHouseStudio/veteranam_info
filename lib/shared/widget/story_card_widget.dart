4992c9d61ece45c81e0be8ed086a468ff7c957af
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-05-17 16:52:06 +0300

    fix story card

--- lib/shared/widget/story_card_widget.dart
+++ lib/shared/widget/story_card_widget.dart
@@ -53,11 +53,10 @@ class StoryCardWidget extends StatelessWidget {
         titleDate: Text(
           storyModel.date.localeTime,
           key: KWidgetkeys.widget.storyCard.date,
-          style: AppTextStyle.text16.copyWith(
-              // color: AppColors.lightGray,
-              ),
+          style: context.textStyle.hint16,
         ),
         isDesk: isDesk,
+        titleIcon: KIcon.trash,
       ),
     );
   }
