049c7db5b21f9ca4231ae94311cb3fec1ef6bdee
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-17 14:34:47 +0300

    update tests

--- test/profile/helper/buttons/log_out_confirm_button_helper.dart
+++ test/profile/helper/buttons/log_out_confirm_button_helper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kozak/components/components.dart';
 import 'package:kozak/shared/constants/constants.dart';
 
 import '../../../text_dependency.dart';
@@ -8,11 +7,6 @@ Future<void> logOutConfirmButtonlHelper(
   WidgetTester tester,
   MockGoRouter mockGoRouter,
 ) async {
-  await tester.pumpApp(
-    const ProfileScreen(),
-    mockGoRouter: mockGoRouter,
-  );
-
   expect(
     find.byKey(KWidgetkeys.screen.profile.screen),
     findsOneWidget,
--- test/profile/profile_widget_test.dart
+++ test/profile/profile_widget_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
-import 'package:kozak/components/components.dart';
-import 'package:kozak/shared/shared.dart';
 import '../text_dependency.dart';
 
 import 'helper/helper.dart';
@@ -16,22 +14,18 @@ void main() {
   tearDown(GetIt.I.reset);
   group('${KScreenBlocName.profile} ', () {
     testWidgets('${KGroupText.intial} ', (tester) async {
-      await tester.pumpApp(const ProfileScreen());
-      expect(
-        find.byKey(KWidgetkeys.screen.profile.screen),
-        findsOneWidget,
-      );
+      await profilePumpAppHelper(tester: tester);
 
-      await tester.pumpAndSettle();
+      await profileInitialHelper(tester);
     });
     testWidgets('Show log out dialog', (tester) async {
-      await tester.pumpApp(const ProfileScreen());
+      await profilePumpAppHelper(tester: tester);
 
       await logOutDialoglHelper(tester);
     });
 
     testWidgets('Show delete account dialog', (tester) async {
-      await tester.pumpApp(const ProfileScreen());
+      await profilePumpAppHelper(tester: tester);
 
       await deleteAccountDialoglHelper(tester);
     });
@@ -40,26 +34,49 @@ void main() {
       late MockGoRouter mockGoRouter;
       setUp(() => mockGoRouter = MockGoRouter());
       testWidgets('${KGroupText.intial} ', (tester) async {
-        await tester.pumpApp(const ProfileScreen(), mockGoRouter: mockGoRouter);
+        await profilePumpAppHelper(
+          tester: tester,
+          mockGoRouter: mockGoRouter,
+        );
 
         await profileInitialHelper(tester);
       });
 
       group('${KGroupText.goTo} ', () {
         testWidgets('Log out dialog unconfirm button pop', (tester) async {
+          await profilePumpAppHelper(
+            tester: tester,
+            mockGoRouter: mockGoRouter,
+          );
+
           await logOutUnconfirmButtonlHelper(tester, mockGoRouter);
         });
 
         testWidgets('Log out dialog confirm button pop', (tester) async {
+          await profilePumpAppHelper(
+            tester: tester,
+            mockGoRouter: mockGoRouter,
+          );
+
           await logOutConfirmButtonlHelper(tester, mockGoRouter);
         });
 
         testWidgets('Delete account dialog unconfirm button pop',
             (tester) async {
+          await profilePumpAppHelper(
+            tester: tester,
+            mockGoRouter: mockGoRouter,
+          );
+
           await deleteAccountUnconfirmButtonlHelper(tester, mockGoRouter);
         });
 
         testWidgets('Delete account dialog confirm button pop', (tester) async {
+          await profilePumpAppHelper(
+            tester: tester,
+            mockGoRouter: mockGoRouter,
+          );
+
           await deleteAccountConfirmButtonlHelper(tester, mockGoRouter);
         });
       });
