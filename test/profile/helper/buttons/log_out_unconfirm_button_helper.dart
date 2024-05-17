0d5ad44986555cbc1c21dab0b3e171c710e3acc7
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-17 14:34:47 +0300

    update tests

--- test/profile/helper/buttons/log_out_unconfirm_button_helper.dart
+++ test/profile/helper/buttons/log_out_unconfirm_button_helper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kozak/components/components.dart';
 import 'package:kozak/shared/constants/constants.dart';
 
 import '../../../text_dependency.dart';
@@ -8,10 +7,6 @@ Future<void> logOutUnconfirmButtonlHelper(
   WidgetTester tester,
   MockGoRouter mockGoRouter,
 ) async {
-  await tester.pumpApp(
-    const ProfileScreen(),
-    mockGoRouter: mockGoRouter,
-  );
   expect(
     find.byKey(KWidgetkeys.screen.profile.screen),
     findsOneWidget,
