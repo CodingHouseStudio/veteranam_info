3bd7cfc4fb3c6014f88bfc8ccfdedd57a26db657
miq80 <mikoladobriy@gmail.com>
2024-05-21 20:43:20 +0300

    bloc files added

---
+++ lib/components/employee_respond/bloc/employee_respond_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'employee_respond_event.dart';
+part 'employee_respond_state.dart';
+part 'employee_respond_bloc.freezed.dart';
+
+class EmployeeRespondBloc
+    extends Bloc<EmployeeRespondEvent, EmployeeRespondState> {
+  EmployeeRespondBloc() : super(const _Initial()) {
+    on<EmployeeRespondEvent>((event, emit) {});
+  }
+}
