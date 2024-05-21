9c9e65409e8bcce448be88b1b5669add60ff94ab
miq80 <mikoladobriy@gmail.com>
2024-05-21 20:43:20 +0300

    bloc files added

---
+++ lib/components/employee_respond/bloc/employee_respond_event.dart
@@ -0,0 +1,6 @@
+part of 'employee_respond_bloc.dart';
+
+@freezed
+class EmployeeRespondEvent with _$EmployeeRespondEvent {
+  const factory EmployeeRespondEvent.started() = _Started;
+}
---
+++ lib/components/employee_respond/bloc/employee_respond_state.dart
@@ -0,0 +1,6 @@
+part of 'employee_respond_bloc.dart';
+
+@freezed
+class EmployeeRespondState with _$EmployeeRespondState {
+  const factory EmployeeRespondState.initial() = _Initial;
+}
