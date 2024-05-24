5ff4724dfae76c7f19d0a3ad70c6bbccb1997006
Sophia Kolpakova <kolpakova2004@gmail.com>
2024-05-24 14:56:33 +0300

    fix: fix discount and conflicts

--- lib/components/profile_saves/widget/body/profile_saves_body.dart
+++ lib/components/profile_saves/widget/body/profile_saves_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kozak/components/discounts/bloc/discount_watcher_bloc.dart';
 import 'package:kozak/shared/shared.dart';
 
 class ProfileSavesBody extends StatelessWidget {
@@ -6,55 +8,61 @@ class ProfileSavesBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldWidget(
-      titleChildWidgetsFunction: ({required isDesk}) => [
-        if (isDesk)
-          KSizedBox.kHeightSizedBox40
-        else
-          KSizedBox.kHeightSizedBox24,
-        ...TitleWidget.titleWidgetList(
-          title: context.l10n.saved,
-          titleKey: KWidgetkeys.screen.profileSaves.title,
-          subtitle: context.l10n.savesSubtitle,
-          subtitleKey: KWidgetkeys.screen.profileSaves.subtitle,
-          isDesk: isDesk,
-        ),
-        if (isDesk)
-          KSizedBox.kHeightSizedBox56
-        else
-          KSizedBox.kHeightSizedBox24,
-      ],
-      mainDeskPadding: const EdgeInsets.symmetric(
-        horizontal: KPadding.kPaddingSize220,
-      ),
-      mainChildWidgetsFunction: ({required isDesk}) => [
-        DiscountsCardWidget(
-          isSaved: false,
-          key: KWidgetkeys.screen.profileSaves.discountCard,
-          discount: KMockText.discount,
-          city: KMockText.cityDiscount,
-          service: KMockText.serviceDiscount,
-          description: KMockText.descriptionDiscount,
-          date: KMockText.dateDiscount,
-          instruction: KMockText.instructionDiscount,
-          preInstructionDiscount: KMockText.preInstructionDiscount,
-          isDesk: isDesk,
-        ),
-        if (isDesk)
-          KSizedBox.kHeightSizedBox56
-        else
-          KSizedBox.kHeightSizedBox24,
-        WorkCardWidget(
-          isSaved: false,
-          key: KWidgetkeys.screen.profileSaves.workCard,
-          workModel: KMockText.workModel,
-          isDesk: isDesk,
-        ),
-        if (isDesk)
-          KSizedBox.kHeightSizedBox56
-        else
-          KSizedBox.kHeightSizedBox24,
-      ],
+    return BlocBuilder<DiscountWatcherBloc, DiscountWatcherState>(
+      builder: (context, state) {
+        switch (state.loadingStatus) {
+          case LoadingStatus.initial:
+            return const CircularProgressIndicator.adaptive();
+          case LoadingStatus.loading:
+            return const CircularProgressIndicator.adaptive();
+          case LoadingStatus.error:
+            return const CircularProgressIndicator.adaptive();
+          case LoadingStatus.loaded:
+            return ScaffoldWidget(
+              titleChildWidgetsFunction: ({required isDesk}) => [
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox40
+                else
+                  KSizedBox.kHeightSizedBox24,
+                ...TitleWidget.titleWidgetList(
+                  title: context.l10n.saved,
+                  titleKey: KWidgetkeys.screen.profileSaves.title,
+                  subtitle: context.l10n.savesSubtitle,
+                  subtitleKey: KWidgetkeys.screen.profileSaves.subtitle,
+                  isDesk: isDesk,
+                ),
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox56
+                else
+                  KSizedBox.kHeightSizedBox24,
+              ],
+              mainDeskPadding: const EdgeInsets.symmetric(
+                horizontal: KPadding.kPaddingSize220,
+              ),
+              mainChildWidgetsFunction: ({required isDesk}) => [
+                DiscountsCardWidget(
+                  key: KWidgetkeys.screen.profileSaves.discountCard,
+                  isDesk: isDesk,
+                  discountItem: state.filteredDiscountModelItems.elementAt(0),
+                ),
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox56
+                else
+                  KSizedBox.kHeightSizedBox24,
+                WorkCardWidget(
+                  isSaved: false,
+                  key: KWidgetkeys.screen.profileSaves.workCard,
+                  workModel: KMockText.workModel,
+                  isDesk: isDesk,
+                ),
+                if (isDesk)
+                  KSizedBox.kHeightSizedBox56
+                else
+                  KSizedBox.kHeightSizedBox24,
+              ],
+            );
+        }
+      },
     );
   }
 }
