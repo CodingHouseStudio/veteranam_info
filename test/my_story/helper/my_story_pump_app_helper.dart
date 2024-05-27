1ed85f9f66d63199b7e6f4116c5705e8c9596329
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-27 15:19:07 +0300

    fix test my story page

--- test/my_story/helper/my_story_pump_app_helper.dart
+++ test/my_story/helper/my_story_pump_app_helper.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:kozak/components/components.dart';
 import 'package:kozak/shared/shared.dart';
 
 import '../../text_dependency.dart';
 
 Future<void> myStoryPumpAppHelper({
+  required IStoryRepository mockStoryRepository,
+  required IAppAuthenticationRepository mockAppAuthenticationRepository,
   required WidgetTester tester,
   MockGoRouter? mockGoRouter,
 }) async {
+  _registerMyStoryBloc(
+    mockStoryRepository: mockStoryRepository,
+    mockAppAuthenticationRepository: mockAppAuthenticationRepository,
+  );
+
   await tester.pumpApp(
     const ProfileMyStoryScreen(),
     mockGoRouter: mockGoRouter,
@@ -19,4 +27,20 @@ Future<void> myStoryPumpAppHelper({
   );
 
   await tester.pumpAndSettle();
+}
+
+void _registerMyStoryBloc({
+  required IStoryRepository mockStoryRepository,
+  required IAppAuthenticationRepository mockAppAuthenticationRepository,
+}) {
+  final myStoryWatcherBloc = MyStoryWatcherBloc(
+    storyRepository: mockStoryRepository,
+    iAppAuthenticationRepository: mockAppAuthenticationRepository,
+  );
+  if (GetIt.I.isRegistered<MyStoryWatcherBloc>()) {
+    GetIt.I.unregister<MyStoryWatcherBloc>();
+  }
+  GetIt.I.registerSingleton<MyStoryWatcherBloc>(
+    myStoryWatcherBloc,
+  );
 }
