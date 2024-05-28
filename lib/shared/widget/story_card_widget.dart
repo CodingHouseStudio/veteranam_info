b89854dd61448505f2e6a2fa2af00a7c74979248
Ferrera Marria <marria.ferrera74@gmail.com>
2024-05-28 16:51:09 +0300

    fix

--- lib/shared/widget/story_card_widget.dart
+++ lib/shared/widget/story_card_widget.dart
@@ -6,59 +6,54 @@ class StoryCardWidget extends StatelessWidget {
   const StoryCardWidget({
     required this.storyModel,
     required this.isDesk,
-    this.storyPadding = EdgeInsets.zero,
     super.key,
   });
 
   final StoryModel storyModel;
   final bool isDesk;
-  final EdgeInsets storyPadding;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: storyPadding,
-      child: CardTextDetailEvaluateWidget(
-        image: storyModel.image.firstImage,
-        text: storyModel.story,
-        titleTopMob: true,
-        titleWidget: Row(
-          children: [
-            if (storyModel.userPhoto == null)
-              IconWidget(
-                key: KWidgetkeys.widget.storyCard.userIcon,
-                icon: KIcon.person,
-                background: context.color.onPrimary,
-              )
-            else
-              ClipRRect(
-                borderRadius: BorderRadius.circular(KSize.kUserPhoto),
-                child: CachedNetworkImage(
-                  imageUrl: storyModel.userPhoto.firstImage!,
-                  placeholder: (context, url) => Image.asset(''),
-                  errorWidget: (context, url, error) => KIcon.error,
-                  fit: BoxFit.contain,
-                  width: KSize.kUserPhoto,
-                  height: KSize.kUserPhoto,
-                ),
+    return CardTextDetailEvaluateWidget(
+      image: storyModel.image.firstImage,
+      text: storyModel.story,
+      titleTopMob: true,
+      titleWidget: Row(
+        children: [
+          if (storyModel.userPhoto == null)
+            IconWidget(
+              key: KWidgetkeys.widget.storyCard.userIcon,
+              icon: KIcon.person,
+              background: context.color.onPrimary,
+            )
+          else
+            ClipRRect(
+              borderRadius: BorderRadius.circular(KSize.kUserPhoto),
+              child: CachedNetworkImage(
+                imageUrl: storyModel.userPhoto.firstImage!,
+                placeholder: (context, url) => Image.asset(''),
+                errorWidget: (context, url, error) => KIcon.error,
+                fit: BoxFit.contain,
+                width: KSize.kUserPhoto,
+                height: KSize.kUserPhoto,
               ),
-            KSizedBox.kWidthSizedBox8,
-            Text(
-              storyModel.userName ?? context.l10n.anonymous,
-              key: KWidgetkeys.widget.storyCard.userName,
-              style: AppTextStyle.text14,
             ),
-          ],
-        ),
-        titleDate: Text(
-          storyModel.date.localeTime,
-          key: KWidgetkeys.widget.storyCard.date,
-          style: context.textStyle.hint16,
-        ),
-        isDesk: isDesk,
-        titleIcon: KIcon.trash.setIconKey(
-          KWidgetkeys.widget.storyCard.trashIcon,
-        ),
+          KSizedBox.kWidthSizedBox8,
+          Text(
+            storyModel.userName ?? context.l10n.anonymous,
+            key: KWidgetkeys.widget.storyCard.userName,
+            style: AppTextStyle.text14,
+          ),
+        ],
+      ),
+      titleDate: Text(
+        storyModel.date.localeTime,
+        key: KWidgetkeys.widget.storyCard.date,
+        style: context.textStyle.hint16,
+      ),
+      isDesk: isDesk,
+      titleIcon: KIcon.trash.setIconKey(
+        KWidgetkeys.widget.storyCard.trashIcon,
       ),
     );
   }
