09dda7c04ff1dfe48a2401b25fe8cdb7229fc09e
miq80 <mikoladobriy@gmail.com>
2024-05-31 11:50:46 +0300

    resume validation added

--- lib/components/employee_respond/bloc/employee_respond_event.dart
+++ lib/components/employee_respond/bloc/employee_respond_event.dart
@@ -5,4 +5,6 @@ class EmployeeRespondEvent with _$EmployeeRespondEvent {
   const factory EmployeeRespondEvent.emailUpdated(String email) = _EmailUpdated;
   const factory EmployeeRespondEvent.phoneUpdated(int phone) = _PhoneUpdated;
   const factory EmployeeRespondEvent.save() = _Save;
+  const factory EmployeeRespondEvent.noResumeChanged({required bool noResume}) =
+      _NoResumeChanged;
 }
