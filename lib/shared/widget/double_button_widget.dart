94d7163d52ca1867153a50bdb236563ebb2130f8
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-06-03 08:05:58 +0300

    double button animation

--- lib/shared/widget/double_button_widget.dart
+++ lib/shared/widget/double_button_widget.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:kozak/shared/shared.dart';
 
-class DoubleButtonWidget extends StatelessWidget {
+class DoubleButtonWidget extends StatefulWidget {
   const DoubleButtonWidget({
     required this.text,
     required this.onPressed,
@@ -15,33 +17,69 @@ class DoubleButtonWidget extends StatelessWidget {
   final void Function()? onPressed;
 
   @override
+  State<DoubleButtonWidget> createState() => _DoubleButtonWidgetState();
+}
+
+class _DoubleButtonWidgetState extends State<DoubleButtonWidget> {
+  late bool isHovering;
+  @override
+  void initState() {
+    isHovering = false;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return IntrinsicWidth(
+    return Align(
+      alignment: Alignment.centerLeft,
       child: TextButton(
-        onPressed: onPressed,
-        style: KButtonStyles.zeroPaddingButtonStyle,
+        onPressed: widget.onPressed,
+        style: KButtonStyles.doubleButtonStyle,
+        onHover: (value) => setState(() {
+          isHovering = value;
+        }),
         child: Stack(
           alignment: Alignment.centerRight,
           children: [
-            Container(
-              margin: const EdgeInsets.only(right: KPadding.kPaddingSize40),
-              decoration:
-                  KWidgetTheme.boxDecorationBlack.copyWith(color: color),
-              padding: const EdgeInsets.symmetric(
-                horizontal: KPadding.kPaddingSize30,
-                vertical: KPadding.kPaddingSize12,
-              ),
-              child: Text(
-                text,
-                style: AppTextStyle.materialThemeTitleMedium.copyWith(
-                  color: textColor ?? AppColors.materialThemeBlack,
+            AnimatedPadding(
+              duration: const Duration(milliseconds: 200),
+              padding: isHovering
+                  ? const EdgeInsets.only(right: KPadding.kPaddingSize12)
+                  : EdgeInsets.zero,
+              child: Container(
+                margin: const EdgeInsets.only(right: KPadding.kPaddingSize40),
+                decoration: KWidgetTheme.boxDecorationBlack
+                    .copyWith(color: widget.color),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: KPadding.kPaddingSize30,
+                  vertical: KPadding.kPaddingSize12,
+                ),
+                child: Text(
+                  widget.text,
+                  style: AppTextStyle.materialThemeTitleMedium.copyWith(
+                    color: widget.textColor ?? AppColors.materialThemeBlack,
+                  ),
                 ),
               ),
             ),
-            IconWidget(
-              icon: KIcon.arrowUpRight
-                  .copyWith(color: textColor ?? AppColors.materialThemeBlack),
-              background: color ?? AppColors.materialThemeSourceSeed,
+            TweenAnimationBuilder<double>(
+              tween: Tween<double>(
+                begin: 0,
+                end: isHovering ? pi / 4 : 0,
+              ),
+              duration: const Duration(milliseconds: 200),
+              builder: (BuildContext context, double angle, Widget? child) {
+                return Transform.rotate(
+                  angle: angle,
+                  child: child,
+                );
+              },
+              child: IconWidget(
+                icon: KIcon.arrowUpRight.copyWith(
+                  color: widget.textColor ?? AppColors.materialThemeBlack,
+                ),
+                background: widget.color ?? AppColors.materialThemeSourceSeed,
+              ),
             ),
           ],
         ),
