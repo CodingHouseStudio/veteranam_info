1372ea8a4018c98291290aef911538dd2dcd8dd3
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-06-10 10:54:24 +0300

    temporary

--- lib/shared/widget/title_widget_list.dart
+++ lib/shared/widget/title_widget_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:kozak/shared/shared.dart';
 
 abstract class TitleWidget {
@@ -22,4 +23,59 @@ abstract class TitleWidget {
           style: isDesk ? AppTextStyle.text24 : AppTextStyle.text16,
         ),
       ];
+  static List<Widget> pointTitleWidgetList({
+    required String title,
+    required String titleSecondPart,
+    required Key titleKey,
+    required String pointText,
+    required Key pointKey,
+    required bool isDesk,
+    CrossAxisAlignment titleCrossAxisAlignmentm = CrossAxisAlignment.stretch,
+  }) =>
+      isDesk
+          ? [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextPointWidget(
+                      pointText,
+                      key: pointKey,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      key: titleKey,
+                      crossAxisAlignment: titleCrossAxisAlignmentm,
+                      children: [
+                        Text(
+                          title,
+                          style: AppTextStyle.materialThemeDisplayLarge,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              titleSecondPart,
+                              style: AppTextStyle.materialThemeDisplayLarge,
+                            ),
+                            KSizedBox.kWidthSizedBox24,
+                            IconWidget(
+                              icon: KIcon.arrowDownRight.copyWith(
+                                key: KWidgetkeys.screen.feedback.titleIcon,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              KSizedBox.kHeightSizedBox32,
+              Divider(
+                key: KWidgetkeys.screen.feedback.divider,
+                color: AppColors.materialThemeKeyColorsNeutral,
+              ),
+            ]
+          : [];
 }
