116291e9e1dece7c202d4f70397ccf026c7ffa53
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-06-14 15:23:19 +0300

    feat: homa page only has footer (#281)
    * homa page only has footer
    
    * fix integration test

--- test/story/helper/story_initial_helper.dart
+++ test/story/helper/story_initial_helper.dart
@@ -36,10 +36,10 @@ Future<void> storyInitialHelper(
         offset: KTestConstants.scrollingDown,
       );
 
-      await scrollingHelper(
-        tester: tester,
-        offset: KTestConstants.scrollingUp500,
-      );
+      // await scrollingHelper(
+      //   tester: tester,
+      //   offset: KTestConstants.scrollingUp500,
+      // );
 
       expect(
         find.byKey(KWidgetkeys.screen.story.button),
