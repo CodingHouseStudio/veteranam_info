d38bc9d39a8bb897a4bac17b369a1b9dfa1f5be3
Sophia Kolpakova <kolpakova2004@gmail.com>
2024-06-21 14:21:22 +0300

    feat: fix news card

--- lib/shared/constants/text/text_constants.dart
+++ lib/shared/constants/text/text_constants.dart
@@ -161,11 +161,9 @@ abstract class KMockText {
     topic: 'MEDICAL BENEFITS',
     topicUA: 'МЕДИЧНІ ПІЛЬГИ',
     status: 'Healthy',
-    link:
-        'https://wiki.legalaid.gov.ua/index.php/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D1%96%D1%8F:%D0%92%D0%B5%D1%82%D0%B5%D1%80%D0%B0%D0%BD%D0%B8_%D0%B2%D1%96%D0%B9%D0%BD%D0%B8_%D1%82%D0%B0_%D1%83%D1%87%D0%B0%D1%81%D0%BD%D0%B8%D0%BA%D0%B8_%D0%90%D0%A2%D0%9E',
+    link: '',
     fetchDate: DateTime(2024, 06, 07),
-    directLink:
-        'https://wiki.legalaid.gov.ua/index.php/%D0%9F%D1%96%D0%BB%D1%8C%D0%B3%D0%B8_%D1%83%D1%87%D0%B0%D1%81%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC_%D0%B1%D0%BE%D0%B9%D0%BE%D0%B2%D0%B8%D1%85_%D0%B4%D1%96%D0%B9_%D1%89%D0%BE%D0%B4%D0%BE_%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%B8_%D0%BF%D0%BE%D0%B4%D0%B0%D1%82%D0%BA%D1%96%D0%B2_%D1%82%D0%B0_%D0%B7%D0%B1%D0%BE%D1%80%D1%96%D0%B2',
+    directLink: '',
   );
   static const fundModel = FundModel(
     id: '',
