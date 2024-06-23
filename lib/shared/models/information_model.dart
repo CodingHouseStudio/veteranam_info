5435197b5c815b4952318c494f52f94f8b1cd285
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-06-23 07:14:15 +0300

    test: improvment test coverage (#315)
    * improvment tests
    
    * impovment tests
    
    * change image convertor
    
    * image test

--- lib/shared/models/information_model.dart
+++ lib/shared/models/information_model.dart
@@ -17,7 +17,7 @@ class InformationModel with _$InformationModel {
     required String topicUA,
     required String status,
     required DateTime fetchDate,
-    @ImageConverter() List<ImageModel>? image,
+    @ImageConverter() ImageModel? image,
   }) = _InformationModel;
 
   factory InformationModel.fromJson(Map<String, dynamic> json) =>
