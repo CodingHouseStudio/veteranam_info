c564f25f333ab94b072b06a9fce349da8f04ee55
Sophia Kolpakova <kolpakova2004@gmail.com>
2024-06-25 13:13:48 +0300

    fix: fix icon like

--- lib/shared/models/information_model.dart
+++ lib/shared/models/information_model.dart
@@ -17,8 +17,6 @@ class InformationModel with _$InformationModel {
     required String topicUA,
     required String status,
     required DateTime fetchDate,
-    String? link,
-    String? directLink,
     @ImageConverter() List<ImageModel>? image,
   }) = _InformationModel;
 
