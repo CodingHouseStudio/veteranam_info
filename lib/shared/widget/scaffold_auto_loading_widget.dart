b83460917dc2b8ff28cfff5d6409f65700a425df
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-06-27 16:53:55 +0300

    feat: desk loading button (#326)
    * desk loading button
    
    * code refactoring
    
    * fix tests

--- lib/shared/widget/scaffold_auto_loading_widget.dart
+++ lib/shared/widget/scaffold_auto_loading_widget.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:kozak/shared/shared.dart';
 
-class ScaffoldAutoLoadingWidget extends StatefulWidget {
+class ScaffoldAutoLoadingWidget extends StatelessWidget {
   const ScaffoldAutoLoadingWidget({
     required this.mainChildWidgetsFunction,
     required this.scrollFunction,
-    this.titleChildWidgetsFunction,
+    required this.loadingButtonText,
     super.key,
+    this.titleChildWidgetsFunction,
+    this.mainDeskPadding,
+    this.mainRightChildWidget,
+  });
+  final List<Widget> Function({required bool isDesk})?
+      titleChildWidgetsFunction;
+  final List<Widget> Function({required bool isDesk}) mainChildWidgetsFunction;
+  final EdgeInsetsGeometry? mainDeskPadding;
+  final void Function() scrollFunction;
+  final Widget? mainRightChildWidget;
+  final String loadingButtonText;
+
+  @override
+  Widget build(BuildContext context) {
+    if (KPlatformConstants.isWebDesktop) {
+      return _ScaffoldAutoLoadingWidgetDesk(
+        mainChildWidgetsFunction: mainChildWidgetsFunction,
+        loadFunction: scrollFunction,
+        mainDeskPadding: mainDeskPadding,
+        mainRightChildWidget: mainRightChildWidget,
+        titleChildWidgetsFunction: titleChildWidgetsFunction,
+        loadingButtonText: loadingButtonText,
+      );
+    } else {
+      return _ScaffoldAutoLoadingWidgetMobile(
+        mainChildWidgetsFunction: mainChildWidgetsFunction,
+        scrollFunction: scrollFunction,
+        mainDeskPadding: mainDeskPadding,
+        mainRightChildWidget: mainRightChildWidget,
+        titleChildWidgetsFunction: titleChildWidgetsFunction,
+      );
+    }
+  }
+}
+
+class _ScaffoldAutoLoadingWidgetMobile extends StatefulWidget {
+  const _ScaffoldAutoLoadingWidgetMobile({
+    required this.mainChildWidgetsFunction,
+    required this.scrollFunction,
+    this.titleChildWidgetsFunction,
     this.mainDeskPadding,
     this.mainRightChildWidget,
   });
@@ -18,15 +58,17 @@ class ScaffoldAutoLoadingWidget extends StatefulWidget {
   final Widget? mainRightChildWidget;
 
   @override
-  State<ScaffoldAutoLoadingWidget> createState() =>
-      _ScaffoldAutoLoadingWidgetState();
+  State<_ScaffoldAutoLoadingWidgetMobile> createState() =>
+      _ScaffoldAutoLoadingWidgetMobileState();
 }
 
-class _ScaffoldAutoLoadingWidgetState extends State<ScaffoldAutoLoadingWidget> {
-  final _scrollController = ScrollController();
+class _ScaffoldAutoLoadingWidgetMobileState
+    extends State<_ScaffoldAutoLoadingWidgetMobile> {
+  late ScrollController _scrollController;
 
   @override
   void initState() {
+    _scrollController = ScrollController();
     super.initState();
     _scrollController.addListener(_onScroll);
   }
@@ -157,4 +199,138 @@ class _ScaffoldAutoLoadingWidgetState extends State<ScaffoldAutoLoadingWidget> {
     final currentScroll = _scrollController.offset;
     return currentScroll >= (maxScroll * 0.9);
   }
+}
+
+class _ScaffoldAutoLoadingWidgetDesk extends StatelessWidget {
+  const _ScaffoldAutoLoadingWidgetDesk({
+    required this.mainChildWidgetsFunction,
+    required this.loadFunction,
+    required this.loadingButtonText,
+    this.titleChildWidgetsFunction,
+    this.mainDeskPadding,
+    this.mainRightChildWidget,
+  });
+  final List<Widget> Function({required bool isDesk})?
+      titleChildWidgetsFunction;
+  final List<Widget> Function({required bool isDesk}) mainChildWidgetsFunction;
+  final EdgeInsetsGeometry? mainDeskPadding;
+  final void Function() loadFunction;
+  final Widget? mainRightChildWidget;
+  final String loadingButtonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final isDesk =
+            constraints.maxWidth > KPlatformConstants.minWidthThresholdTablet;
+        final mainChildWidget = mainChildWidgetsFunction(isDesk: isDesk)
+          ..addAll([
+            LoadingButton(
+              isDesk: isDesk,
+              onPressed: loadFunction,
+              text: loadingButtonText,
+              widgetKey: KWidgetkeys.widget.scaffold.loadingButton,
+            ),
+            KSizedBox.kHeightSizedBox40,
+          ]);
+        final padding = EdgeInsets.symmetric(
+          horizontal: (isDesk
+              ? KPadding.kPaddingSize90 +
+                  ((constraints.maxWidth >
+                          KPlatformConstants.maxWidthThresholdTablet)
+                      ? (constraints.maxWidth -
+                              KPlatformConstants.maxWidthThresholdTablet) /
+                          2
+                      : 0)
+              : KPadding.kPaddingSize16),
+        );
+        // final footerList = FooterWidget.get(
+        //   context: context,
+        //   isDesk: isDesk,
+        // );
+        return Scaffold(
+          body: CustomScrollView(
+            key: KWidgetkeys.widget.scaffold.scroll,
+            slivers: [
+              SliverPersistentHeader(
+                delegate: NawbarWidget(
+                  isDesk: isDesk,
+                ),
+              ),
+              if (titleChildWidgetsFunction != null)
+                SliverPadding(
+                  padding: padding,
+                  sliver: SliverList.builder(
+                    addAutomaticKeepAlives: false,
+                    addRepaintBoundaries: false,
+                    itemBuilder: (context, index) {
+                      return titleChildWidgetsFunction!(isDesk: isDesk)
+                          .elementAt(index);
+                    },
+                    itemCount:
+                        titleChildWidgetsFunction!(isDesk: isDesk).length,
+                  ),
+                ),
+
+              SliverPadding(
+                padding: isDesk && mainDeskPadding != null
+                    ? padding.add(mainDeskPadding!)
+                    : padding,
+                sliver: mainRightChildWidget != null && isDesk
+                    ? RowSliver(
+                        right: mainBody(mainChildWidget),
+                        left: SliverPersistentHeader(
+                          pinned: true,
+                          delegate: NawbarWidget(
+                            isDesk: isDesk,
+                            childWidget: mainRightChildWidget,
+                            maxMinHeight: constraints.maxHeight,
+                          ),
+                        ),
+                        leftWidthPercent: 0.3,
+                      )
+                    : mainBody(mainChildWidget),
+              ),
+              // SliverPadding(
+              //   padding: padding.copyWith(
+              //     bottom: KPadding.kPaddingSize40,
+              //   ),
+              //   sliver: DecoratedSliver(
+              //     decoration: KWidgetTheme.boxDecorationFooter,
+              //     sliver: SliverPadding(
+              //       padding: isDesk
+              //           ? const EdgeInsets.all(KPadding.kPaddingSize32)
+              //               .copyWith(left: KPadding.kPaddingSize46)
+              //           : const EdgeInsets.symmetric(
+              //               vertical: KPadding.kPaddingSize32,
+              //               horizontal: KPadding.kPaddingSize16,
+              //             ),
+              //       sliver: SliverList.builder(
+              //         key: KWidgetkeys.widget.footer.widget,
+              //         addAutomaticKeepAlives: false,
+              //         addRepaintBoundaries: false,
+              //         itemBuilder: (context, index) =>
+              //             footerWidget.elementAt(index),
+              //         itemCount: footerWidget.length,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+            ],
+            semanticChildCount: mainChildWidget.length + 1,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget mainBody(List<Widget> mainChildWidget) => SliverList.builder(
+        addAutomaticKeepAlives: false,
+        addRepaintBoundaries: false,
+        itemBuilder: (context, index) {
+          return mainChildWidget.elementAt(index);
+        },
+        itemCount: mainChildWidget.length,
+      );
 }
