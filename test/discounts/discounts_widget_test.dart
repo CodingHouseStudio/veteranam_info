12b81bea275333b6aa533a947ae3a9a10d7c1987
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-06-27 18:52:18 +0300

    merge

--- test/discounts/discounts_widget_test.dart
+++ test/discounts/discounts_widget_test.dart
@@ -79,13 +79,13 @@ void main() {
         await discountInitialHelper(tester);
       });
 
-      // loadingList(
-      //   pumpApp: (tester) async => discountsPumpAppHelper(
-      //     tester: tester,
-      //     mockDiscountRepository: mockDiscountRepository,
-      //   ),
-      //   lastCard: KWidgetkeys.screen.discounts.cardLast,
-      // );
+      loadingListSeparate(
+        pumpApp: (tester) async => discountsPumpAppHelper(
+          tester: tester,
+          mockDiscountRepository: mockDiscountRepository,
+        ),
+        lastCard: KWidgetkeys.screen.discounts.cardLast,
+      );
 
       group('${KGroupText.goRouter} ', () {
         late MockGoRouter mockGoRouter;
