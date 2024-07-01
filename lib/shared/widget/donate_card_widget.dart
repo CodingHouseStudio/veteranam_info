f2c3981f2913bab817486f6ad986ad1a8da698f0
miq80 <mikoladobriy@gmail.com>
2024-07-01 12:09:25 +0300

    mobile padding fixed

--- lib/shared/widget/donate_card_widget.dart
+++ lib/shared/widget/donate_card_widget.dart
@@ -47,9 +47,11 @@ class DonateCardWidget extends StatelessWidget {
                   : const SizedBox.shrink(),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: KPadding.kPaddingSize16,
-              ),
+              padding: isDesk
+                  ? const EdgeInsets.symmetric(
+                      horizontal: KPadding.kPaddingSize16,
+                    )
+                  : EdgeInsets.zero,
               child: Text(
                 fundModel.title,
                 key: KWidgetkeys.widget.donateCard.title,
@@ -58,8 +60,8 @@ class DonateCardWidget extends StatelessWidget {
             ),
             if (hasSubtitle)
               Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: KPadding.kPaddingSize16,
+                padding: EdgeInsets.symmetric(
+                  horizontal: isDesk ? KPadding.kPaddingSize16 : 0,
                   vertical: KPadding.kPaddingSize16,
                 ),
                 child: Text(
