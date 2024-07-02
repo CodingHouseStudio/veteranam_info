c3932a9038c118ebfdf0c9306a822b999ee84fef
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-07-02 09:55:17 +0300

    merge

--- lib/shared/widget/discount_card_widget.dart
+++ lib/shared/widget/discount_card_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:kozak/shared/shared.dart';
+import 'package:veteranam/shared/shared.dart';
 
 class DiscountsCardWidget extends StatelessWidget {
   const DiscountsCardWidget({
