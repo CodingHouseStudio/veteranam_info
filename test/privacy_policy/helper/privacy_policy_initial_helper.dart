7545bc1c14bb707a74e1acaa43e41fb0c84189d4
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-07-15 18:15:00 +0300

    privacy policy dialog (#371)

---
+++ test/privacy_policy/helper/privacy_policy_initial_helper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:veteranam/shared/shared.dart';
+
+Future<void> privacyPolicyInitialHelper(
+  WidgetTester tester,
+) async {
+  expect(
+    find.byKey(KWidgetkeys.screen.privacyPolicy.text),
+    findsWidgets,
+  );
+
+  expect(
+    find.byKey(KWidgetkeys.screen.privacyPolicy.icon),
+    findsWidgets,
+  );
+}
---
+++ test/privacy_policy/privacy_policy_widget_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import '../text_dependency.dart';
+
+import 'helper/helper.dart';
+
+void main() {
+  setUp(configureDependenciesTest);
+
+  setUpAll(setUpGlobal);
+
+  setupFirebaseAuthMocks();
+
+  tearDown(GetIt.I.reset);
+  group('${KScreenBlocName.privacyPolicy} ', () {
+    testWidgets('${KGroupText.intial} ', (tester) async {
+      await privacyPolicyPumpAppHelper(tester: tester);
+
+      await privacyPolicyInitialHelper(tester);
+    });
+
+    group('${KGroupText.goRouter} ', () {
+      late MockGoRouter mockGoRouter;
+      setUp(() => mockGoRouter = MockGoRouter());
+      testWidgets('${KGroupText.intial} ', (tester) async {
+        await privacyPolicyPumpAppHelper(
+          tester: tester,
+          mockGoRouter: mockGoRouter,
+        );
+
+        await privacyPolicyInitialHelper(tester);
+      });
+      group('${KGroupText.goTo} ', () {
+        testWidgets('pop', (tester) async {
+          await privacyPolicyPumpAppHelper(
+            tester: tester,
+            mockGoRouter: mockGoRouter,
+          );
+
+          await cancelHelper(tester: tester, mockGoRouter: mockGoRouter);
+        });
+      });
+    });
+  });
+}
