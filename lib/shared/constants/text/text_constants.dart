908884b6e5848a6c7ada305d47b8b2d10bd38410
herman-the-worm <49967581+herman-the-worm@users.noreply.github.com>
2024-07-16 03:48:18 -0600

    Update text_constants.dart

--- lib/shared/constants/text/text_constants.dart
+++ lib/shared/constants/text/text_constants.dart
@@ -209,7 +209,7 @@ abstract class KAppText {
   static const String authChange = 'AuthenticationStatusChanged:';
   static const String backendString = '';
   static const String usernameToken = 'USERNAME_TOKEN';
-  static const String madeBy = '© MadeByCodingHouseLLC 2024';
+  static const String madeBy = '© Coding House Studio LLC - 2024';
   static const String email = 'hello@veteranam.info';
   static const String veteranamName = 'veteranam.info';
   static const String linkedIn = 'https://www.linkedin.com/company/veteranam/';
