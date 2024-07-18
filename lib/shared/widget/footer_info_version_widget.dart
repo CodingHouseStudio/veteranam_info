a88794e2954dc522709316a40a8c1f48a807ab70
Mykola Dobrianskyi <mikoladobriy@gmail.com>
2024-07-18 17:39:24 +0300

    feat: add package_info_plus (#387)
    * info plus package added
    
    * version and build number added
    
    * fixes
    
    ---------
    
    Co-authored-by: DemianKryk <158501490+DemianKryk@users.noreply.github.com>

---
+++ lib/shared/widget/footer_info_version_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:veteranam/shared/shared.dart';
+
+class FooterInfoVersionWidget extends StatefulWidget {
+  const FooterInfoVersionWidget({required this.isDesk, super.key});
+  final bool isDesk;
+
+  @override
+  State<FooterInfoVersionWidget> createState() =>
+      _FooterInfoVersionWidgetState();
+}
+
+class _FooterInfoVersionWidgetState extends State<FooterInfoVersionWidget> {
+  late PackageInfo packageInfo;
+
+  @override
+  void initState() {
+    packageInfo = PackageInfo(
+      appName: 'Unknown',
+      packageName: 'Unknown',
+      version: 'Unknown',
+      buildNumber: 'Unknown',
+      buildSignature: 'Unknown',
+      installerStore: 'Unknown',
+    );
+    _initPackageInfo();
+    super.initState();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      packageInfo = info;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '  v${packageInfo.version} build ${packageInfo.buildNumber}',
+      style: widget.isDesk
+          ? AppTextStyle.materialThemeBodyLarge
+          : AppTextStyle.materialThemeBodySmall,
+    );
+  }
+}
