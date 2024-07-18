e873c9c2bd344ea485338b19f0dc6b7d83947fcf
Mykola Dobrianskyi <mikoladobriy@gmail.com>
2024-07-18 17:39:24 +0300

    feat: add package_info_plus (#387)
    * info plus package added
    
    * version and build number added
    
    * fixes
    
    ---------
    
    Co-authored-by: DemianKryk <158501490+DemianKryk@users.noreply.github.com>

--- lib/shared/widget/footer_widget.dart
+++ lib/shared/widget/footer_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:url_launcher/url_launcher.dart';
 import 'package:veteranam/shared/shared.dart';
 
@@ -80,12 +81,12 @@ abstract class FooterWidget {
                   ),
                   Expanded(
                     child: Wrap(
-                      spacing: KPadding.kPaddingSize16,
+                      spacing: KPadding.kPaddingSize8,
                       alignment: WrapAlignment.end,
                       crossAxisAlignment: WrapCrossAlignment.end,
                       children: [
                         Text(
-                          '${KAppText.madeBy}  | ',
+                          '${KAppText.madeBy}  |',
                           key: KWidgetkeys.widget.footer.madeBy,
                           style: AppTextStyle
                               .materialThemeBodyLargeNeutralVariant35,
@@ -100,7 +101,7 @@ abstract class FooterWidget {
                         // ),
                         //KSizedBox.kWidthSizedBox16,
                         Text(
-                          '${context.l10n.allRightsReserved}  | ',
+                          '${context.l10n.allRightsReserved}  |',
                           key: KWidgetkeys.widget.footer.rightReserved,
                           style: AppTextStyle
                               .materialThemeBodyLargeNeutralVariant35,
@@ -118,7 +119,9 @@ abstract class FooterWidget {
                       ],
                     ),
                   ),
-                  KSizedBox.kWidthSizedBox72,
+                  FooterInfoVersionWidget(
+                    isDesk: isDesk,
+                  ),
                 ],
               ),
             ]
@@ -214,6 +217,7 @@ abstract class FooterWidget {
                   Expanded(
                     child: _privacyPolice(context: context, isDesk: false),
                   ),
+                  Expanded(child: FooterInfoVersionWidget(isDesk: isDesk)),
                 ],
               ),
             ];
