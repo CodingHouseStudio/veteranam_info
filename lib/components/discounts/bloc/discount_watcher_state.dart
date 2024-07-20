efc12df3a54aebf39c48a6e975c86d1b5d63ca15
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-07-20 07:15:36 +0300

    selecting a tag changes values in the advances filter (#393)

--- lib/components/discounts/bloc/discount_watcher_state.dart
+++ lib/components/discounts/bloc/discount_watcher_state.dart
@@ -10,6 +10,7 @@ class DiscountWatcherState with _$DiscountWatcherState {
   const factory DiscountWatcherState({
     required List<DiscountModel> discountModelItems,
     required List<DiscountModel> filteredDiscountModelItems,
+    required List<DiscountModel> categoryDiscountModelItems,
     required List<int> filtersCategoriesIndex,
     required List<int> filtersLocationIndex,
     required LoadingStatus loadingStatus,
@@ -20,13 +21,14 @@ class DiscountWatcherState with _$DiscountWatcherState {
 }
 
 extension LocationGetter on List<DiscountModel> {
-  List<dynamic> get _getLocationItems => <dynamic>[
-        '',
-        '',
-        SubLocation.allStoresOfChain,
-        SubLocation.online,
-        ...overallItemBloc(
+  List<FilterItem> get _getLocationItems => <FilterItem>[
+        FilterItem(''),
+        FilterItem(''),
+        FilterItem(SubLocation.allStoresOfChain),
+        FilterItem(SubLocation.online),
+        ...overallItems(
           getFilter: (item) => item.location ?? [],
+          context: null,
         ),
       ];
 }
