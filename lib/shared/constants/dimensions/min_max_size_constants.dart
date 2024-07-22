ba2a78c59595f707e53e5d834adc21d423fff00b
Mykola Dobrianskyi <mikoladobriy@gmail.com>
2024-07-22 17:54:16 +0300

    fix: increase logo quality  (#394)
    * changed logo for better quality
    
    * temp
    
    * return svg image
    
    * fix
    
    ---------
    
    Co-authored-by: DemianKryk <demian.ilnytskyi@codinghouse.biz>

--- lib/shared/constants/dimensions/min_max_size_constants.dart
+++ lib/shared/constants/dimensions/min_max_size_constants.dart
@@ -1,6 +1,7 @@
 abstract class KMinMaxSize {
   static const minWidth100 = 100.0;
   static const minWidth30 = 30.0;
+  static const minWidth78 = 78.0;
   static const maxWidth220 = 220.0;
   static const maxWidth460 = 460.0;
   static const maxWidth600 = 600.0;
