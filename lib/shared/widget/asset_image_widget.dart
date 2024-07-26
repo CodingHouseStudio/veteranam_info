063e838809f5c126eb8d0e3d0f0934a15862ed72
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-07-26 07:21:10 +0300

    feat: asset image widget (#409)
    * asset image widget
    
    * fix tests

---
+++ lib/shared/widget/asset_image_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:veteranam/shared/shared.dart';
+
+class AssetImageWidget extends StatelessWidget {
+  const AssetImageWidget(
+    this.name, {
+    super.key,
+    this.fit,
+    this.width,
+    this.height,
+    this.cacheSize = KMinMaxSize.kImageMaxSize,
+    this.widgetKey,
+  });
+  final String name;
+  final BoxFit? fit;
+  final double? width;
+  final double? height;
+  final int cacheSize;
+  final Key? widgetKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      key: widgetKey ?? Key(name),
+      name,
+      fit: fit,
+      width: width,
+      height: height,
+      cacheHeight: cacheSize,
+      cacheWidth: cacheSize,
+    );
+  }
+}
