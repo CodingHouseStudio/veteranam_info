e42393dd25dba994feecfaa15d978e2debb3ae7c
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-08-01 17:26:55 +0300

    fix privacy policy and nawbar text (#442)

--- lib/components/privacy_policy/widget/body/privacy_policy_body.dart
+++ lib/components/privacy_policy/widget/body/privacy_policy_body.dart
@@ -11,42 +11,50 @@ class PrivacyPolicyBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<PrivacyPolicyMarkdownCubit, String?>(
       builder: (context, _) {
-        return Container(
-          constraints: const BoxConstraints(
-            maxWidth: KMinMaxSize.maxWidth768,
-          ),
-          width: double.infinity,
-          padding: const EdgeInsets.only(
-            right: KPadding.kPaddingSize32,
-            left: KPadding.kPaddingSize32,
-            bottom: KPadding.kPaddingSize32,
-          ),
-          child: _ == null
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    MarkdownBody(
-                      data: context.l10n.privacyPolicyStartText,
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            final isTablet = constraints.maxWidth >
+                KPlatformConstants.minWidthThresholdTablet;
+            return Padding(
+              padding: (isTablet
+                      ? const EdgeInsets.all(
+                          KPadding.kPaddingSize32,
+                        )
+                      : const EdgeInsets.all(
+                          KPadding.kPaddingSize16,
+                        ))
+                  .copyWith(
+                top: KPadding.kPaddingSize60,
+              ),
+              child: _ == null
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MarkdownBody(
+                          data: context.l10n.privacyPolicyStartText,
+                        ),
+                        KSizedBox.kHeightSizedBox32,
+                        const Padding(
+                          padding: EdgeInsets.all(KPadding.kPaddingSize32),
+                          child: Center(
+                            child: CircularProgressIndicator.adaptive(),
+                          ),
+                        ),
+                        KSizedBox.kHeightSizedBox32,
+                      ],
+                    )
+                  : MarkdownBody(
+                      key: KWidgetkeys.screen.privacyPolicy.text,
+                      data: _,
+                      styleSheet: MarkdownStyleSheet(
+                        a: AppTextStyle.materialThemeBodyLarge,
+                      ),
+                      onTapLink: (text, href, title) => context
+                          .read<UrlCubit>()
+                          .launchUrl(url: text, scheme: 'mailto'),
                     ),
-                    KSizedBox.kHeightSizedBox32,
-                    const Padding(
-                      padding: EdgeInsets.all(KPadding.kPaddingSize32),
-                      child:
-                          Center(child: CircularProgressIndicator.adaptive()),
-                    ),
-                    KSizedBox.kHeightSizedBox32,
-                  ],
-                )
-              : MarkdownBody(
-                  key: KWidgetkeys.screen.privacyPolicy.text,
-                  data: _,
-                  styleSheet: MarkdownStyleSheet(
-                    a: AppTextStyle.materialThemeBodyLarge,
-                  ),
-                  onTapLink: (text, href, title) => context
-                      .read<UrlCubit>()
-                      .launchUrl(url: text, scheme: 'mailto'),
-                ),
+            );
+          },
         );
       },
     );
