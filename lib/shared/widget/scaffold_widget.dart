01bda673c07289ed4640283408f04fbd402a6fa5
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-08-01 09:36:13 +0300

    fix analyze

--- lib/shared/widget/scaffold_widget.dart
+++ lib/shared/widget/scaffold_widget.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/gestures.dart';
+// import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:veteranam/shared/shared.dart';
