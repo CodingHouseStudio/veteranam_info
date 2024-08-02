870e5795afc55204d551604bf5f912c08fbe2dce
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-08-02 23:35:04 +0300

    improve coverage

--- test/story/unit_test/story_bloc_test.dart
+++ test/story/unit_test/story_bloc_test.dart
@@ -145,14 +145,14 @@ void main() {
           (state) => state.loadingStatus == LoadingStatus.loaded,
         ),
         predicate<StoryWatcherState>(
-          (state) => state.loadingStatus == LoadingStatus.loading,
-        ),
-        predicate<StoryWatcherState>(
           (state) => state.loadingStatus == LoadingStatus.listLoadedFull,
-          // &&
-          // state.loadingStoryModelItems.length != KDimensions.loadItems &&
-          // state.itemsLoaded != KDimensions.loadItems,
         ),
+        // predicate<StoryWatcherState>(
+        //   (state) => state.loadingStatus == LoadingStatus.listLoadedFull,
+        // &&
+        // state.loadingStoryModelItems.length != KDimensions.loadItems &&
+        // state.itemsLoaded != KDimensions.loadItems,
+        // ),
       ],
     );
 
