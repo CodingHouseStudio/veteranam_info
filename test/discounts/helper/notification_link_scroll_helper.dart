82077ed80e60027c1c3d0dd99ac40ec848ce1c8a
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-08-05 23:23:33 +0300

    fix tests

--- test/discounts/helper/notification_link_scroll_helper.dart
+++ test/discounts/helper/notification_link_scroll_helper.dart
@@ -1,49 +1,44 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:veteranam/shared/shared.dart';
+import 'package:veteranam/shared/constants/widget_keys/widget_keys.dart';
 
 import '../../test_dependency.dart';
 
 Future<void> notificationLinkScrollHelper({
   required WidgetTester tester,
   required Future<void> Function(WidgetTester tester) test,
-  bool scrollUp = false,
-  bool windowsTest = false,
-  Key? itemKey,
 }) async {
-  // await scrollingHelper(
-  //   tester: tester,
-  //   offset: KTestConstants.scrollingUp500,
-  // );
-
   await changeWindowSizeHelper(
     tester: tester,
-    windowsTest: windowsTest,
     test: () async {
-      await tester.dragUntilVisible(
-        find.byKey(itemKey ?? KWidgetkeys.widget.notificationLink.text),
-        find.byKey(KWidgetkeys.widget.scaffold.scroll),
-        KTestConstants.scrollingDown100,
-        maxIteration: 1000,
+      await scrollingHelper(
+        tester: tester,
+        offset: KTestConstants.scrollingDown,
       );
+
+      await tester.tap(find.byKey(KWidgetkeys.widget.scaffold.loadingButton));
 
       await tester.pumpAndSettle();
 
-      // await scrollingHelper(
-      //   tester: tester,
-      //   offset: KTestConstants.scrollingUp200,
-      // );
+      await scrollingHelper(
+        tester: tester,
+        offset: KTestConstants.scrollingDown,
+      );
+
+      await tester.tap(find.byKey(KWidgetkeys.widget.scaffold.loadingButton));
+
+      await tester.pumpAndSettle();
+
+      await scrollingHelper(
+        tester: tester,
+        offset: KTestConstants.scrollingDown,
+      );
 
       await test(tester);
+
+      await scrollingHelper(
+        tester: tester,
+        offset: KTestConstants.scrollingUp,
+      );
     },
   );
-  if (scrollUp) {
-    await tester.dragUntilVisible(
-      find.byKey(
-        KWidgetkeys.screen.discounts.title,
-      ),
-      find.byKey(KWidgetkeys.widget.scaffold.scroll),
-      KTestConstants.scrollingUp,
-    );
-  }
 }
