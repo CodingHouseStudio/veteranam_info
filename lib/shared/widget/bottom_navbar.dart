32b75afbc80722bad6161d62a7f403d8c9968708
Ferrera Marria <marria.ferrera74@gmail.com>
2024-08-05 17:52:26 +0300

    update

--- lib/shared/widget/bottom_navbar.dart
+++ lib/shared/widget/bottom_navbar.dart
@@ -14,28 +14,51 @@ class BottomNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(minHeight: KSize.kPixel60),
-      child: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: KIcon.tag,
-            label: context.l10n.discounts,
-          ),
-          BottomNavigationBarItem(
-            icon: KIcon.briefcase,
-            label: context.l10n.investors,
-          ),
-          BottomNavigationBarItem(
-            icon: KIcon.settings,
-            label: context.l10n.settings,
-          ),
-        ],
-        unselectedLabelStyle: AppTextStyle.materialThemeLabelSmall,
-        selectedLabelStyle: AppTextStyle.materialThemeLabelSmall,
-        currentIndex: index,
-        onTap: (i) => context.goNamed(route.elementAt(i)),
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(KSize.kPixel32),
+        topRight: Radius.circular(KSize.kPixel32),
+      ),
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(minHeight: KSize.kPixel80),
+        child: BottomNavigationBar(
+          backgroundColor: AppColors.materialThemeKeyColorsNeutral,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: _buildIcon(KIcon.tag, 0),
+              label: context.l10n.discounts,
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(KIcon.briefcase, 1),
+              label: context.l10n.investors,
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(KIcon.settings, 2),
+              label: context.l10n.settings,
+            ),
+          ],
+          unselectedLabelStyle: AppTextStyle.materialThemeLabelSmall,
+          selectedLabelStyle: AppTextStyle.materialThemeLabelSmall,
+          currentIndex: index,
+          onTap: (i) => context.goNamed(route.elementAt(i)),
+        ),
       ),
     );
+  }
+
+  Widget _buildIcon(Widget icon, int itemIndex) {
+    return index == itemIndex
+        ? Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColors.materialThemeKeyColorsPrimary,
+            ),
+            padding: const EdgeInsets.all(KPadding.kPaddingSize8),
+            child: icon,
+          )
+        : Padding(
+            padding: const EdgeInsets.only(bottom: KPadding.kPaddingSize8),
+            child: icon,
+          );
   }
 }
