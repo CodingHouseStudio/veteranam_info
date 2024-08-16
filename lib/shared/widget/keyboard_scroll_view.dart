af4db090d92ec79bf6d33dbb80832dfff152af17
Ferrera Marria <marria.ferrera74@gmail.com>
2024-08-16 15:55:29 +0300

    fix bug

--- lib/shared/widget/keyboard_scroll_view.dart
+++ lib/shared/widget/keyboard_scroll_view.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:veteranam/shared/shared.dart';
 
-class KeyboardScrollView extends StatelessWidget {
+class KeyboardScrollView extends StatefulWidget {
   const KeyboardScrollView({
     required this.slivers,
     required this.semanticChildCount,
@@ -13,45 +12,6 @@ class KeyboardScrollView extends StatelessWidget {
     this.physics,
     this.scrollController,
   });
-  final List<Widget> slivers;
-  final int semanticChildCount;
-  final ScrollPhysics? physics;
-  final ScrollController? scrollController;
-  final Key widgetKey;
-  final double maxHeight;
-
-  @override
-  Widget build(BuildContext context) {
-    if (KPlatformConstants.isWebDesktop) {
-      return _KeyboardScrollViewWebDesk(
-        widgetKey: widgetKey,
-        slivers: slivers,
-        semanticChildCount: semanticChildCount,
-        physics: physics,
-        scrollController: scrollController,
-        maxHeight: maxHeight,
-      );
-    } else {
-      return CustomScrollView(
-        key: widgetKey,
-        controller: scrollController,
-        slivers: slivers,
-        physics: physics,
-        semanticChildCount: semanticChildCount,
-      );
-    }
-  }
-}
-
-class _KeyboardScrollViewWebDesk extends StatefulWidget {
-  const _KeyboardScrollViewWebDesk({
-    required this.slivers,
-    required this.semanticChildCount,
-    required this.physics,
-    required this.scrollController,
-    required this.widgetKey,
-    required this.maxHeight,
-  });
 
   final List<Widget> slivers;
   final int semanticChildCount;
@@ -61,69 +21,67 @@ class _KeyboardScrollViewWebDesk extends StatefulWidget {
   final double maxHeight;
 
   @override
-  State<_KeyboardScrollViewWebDesk> createState() =>
-      _KeyboardScrollViewWebDeskState();
+  State<KeyboardScrollView> createState() => _KeyboardScrollViewState();
 }
 
-class _KeyboardScrollViewWebDeskState
-    extends State<_KeyboardScrollViewWebDesk> {
+class _KeyboardScrollViewState extends State<KeyboardScrollView> {
+  late FocusNode _focusNode;
   late ScrollController _controller;
-  Timer? _timer;
 
   @override
   void initState() {
+    _focusNode = FocusNode();
     _controller = widget.scrollController ?? ScrollController();
     super.initState();
   }
 
   @override
   void dispose() {
-    _timer?.cancel();
+    _focusNode.dispose();
     if (widget.scrollController == null) {
       _controller.dispose();
     }
     super.dispose();
   }
 
-  void _startScroll(PhysicalKeyboardKey key) {
+  void _startScroll(LogicalKeyboardKey key) {
     const duration = Duration(milliseconds: 300);
     final step = widget.maxHeight * KSize.kPercentOfScreen;
 
     void scroll() {
       final offset = _controller.offset;
       switch (key) {
-        case PhysicalKeyboardKey.arrowDown:
+        case LogicalKeyboardKey.arrowDown:
           _controller.animateTo(
             offset + KSize.kArrowsStep,
             duration: duration,
             curve: Curves.linear,
           );
-        case PhysicalKeyboardKey.arrowUp:
+        case LogicalKeyboardKey.arrowUp:
           _controller.animateTo(
             offset - KSize.kArrowsStep,
             duration: duration,
             curve: Curves.linear,
           );
-        case PhysicalKeyboardKey.pageDown:
-        case PhysicalKeyboardKey.space:
+        case LogicalKeyboardKey.pageDown:
           _controller.animateTo(
             offset + step,
             duration: duration,
             curve: Curves.linear,
           );
-        case PhysicalKeyboardKey.pageUp:
+        case LogicalKeyboardKey.pageUp:
           _controller.animateTo(
             offset - step,
             duration: duration,
             curve: Curves.linear,
           );
-        case PhysicalKeyboardKey.home:
+        case LogicalKeyboardKey.home:
           _controller.animateTo(
             0,
             duration: duration,
             curve: Curves.linear,
           );
-        case PhysicalKeyboardKey.end:
+        case LogicalKeyboardKey.end:
           _controller.animateTo(
             _controller.position.maxScrollExtent,
             duration: duration,
@@ -135,49 +93,100 @@ class _KeyboardScrollViewWebDeskState
     }
 
     scroll();
-    _timer = Timer.periodic(duration, (timer) => scroll());
-  }
-
-  bool isScrollingKey(PhysicalKeyboardKey key) => [
-        PhysicalKeyboardKey.arrowDown,
-        PhysicalKeyboardKey.arrowUp,
-        PhysicalKeyboardKey.pageDown,
-        PhysicalKeyboardKey.pageUp,
-        PhysicalKeyboardKey.space,
-        PhysicalKeyboardKey.home,
-        PhysicalKeyboardKey.end,
-      ].contains(key);
-
-  void _stopScroll() {
-    _timer?.cancel();
-    _timer = null;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Focus(
-      autofocus: true,
-      onKeyEvent: (FocusNode node, KeyEvent event) {
-        if (isScrollingKey(event.physicalKey)) {
-          if (event is KeyDownEvent) {
-            if (_timer == null) {
-              _startScroll(event.physicalKey);
-            }
-            return KeyEventResult.handled;
-          } else if (event is KeyUpEvent) {
-            _stopScroll();
-            return KeyEventResult.handled;
-          }
-        }
-        return KeyEventResult.ignored;
+    return GestureDetector(
+      onTap: () {
+        _focusNode.requestFocus();
+        setState(() {});
       },
-      child: CustomScrollView(
-        key: widget.widgetKey,
-        controller: _controller,
-        slivers: widget.slivers,
-        physics: widget.physics,
-        semanticChildCount: widget.semanticChildCount,
+      child: FocusableActionDetector(
+        focusNode: _focusNode,
+        shortcuts: {
+          LogicalKeySet(LogicalKeyboardKey.arrowUp): ScrollUpIntent(),
+          LogicalKeySet(LogicalKeyboardKey.arrowDown): ScrollDownIntent(),
+          LogicalKeySet(LogicalKeyboardKey.arrowLeft): ScrollLeftIntent(),
+          LogicalKeySet(LogicalKeyboardKey.arrowRight): ScrollRightIntent(),
+          LogicalKeySet(LogicalKeyboardKey.pageDown): PageDownIntent(),
+          LogicalKeySet(LogicalKeyboardKey.pageUp): PageUpIntent(),
+          LogicalKeySet(LogicalKeyboardKey.home): HomeIntent(),
+          LogicalKeySet(LogicalKeyboardKey.end): EndIntent(),
+        },
+        actions: {
+          ScrollUpIntent: CallbackAction<ScrollUpIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.arrowUp);
+              return;
+            },
+          ),
+          ScrollDownIntent: CallbackAction<ScrollDownIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.arrowDown);
+              return;
+            },
+          ),
+          ScrollRightIntent: CallbackAction<ScrollRightIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.arrowDown);
+              return;
+            },
+          ),
+          ScrollLeftIntent: CallbackAction<ScrollLeftIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.arrowUp);
+              return;
+            },
+          ),
+          PageDownIntent: CallbackAction<PageDownIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.pageDown);
+              return;
+            },
+          ),
+          PageUpIntent: CallbackAction<PageUpIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.pageUp);
+              return;
+            },
+          ),
+          HomeIntent: CallbackAction<HomeIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.home);
+              return;
+            },
+          ),
+          EndIntent: CallbackAction<EndIntent>(
+            onInvoke: (intent) {
+              _startScroll(LogicalKeyboardKey.end);
+              return;
+            },
+          ),
+        },
+        child: CustomScrollView(
+          key: widget.widgetKey,
+          controller: _controller,
+          physics: widget.physics,
+          slivers: widget.slivers,
+        ),
       ),
     );
   }
 }
+
+class ScrollUpIntent extends Intent {}
+
+class ScrollDownIntent extends Intent {}
+
+class ScrollLeftIntent extends Intent {}
+
+class ScrollRightIntent extends Intent {}
+
+class PageDownIntent extends Intent {}
+
+class PageUpIntent extends Intent {}
+
+class HomeIntent extends Intent {}
+
+class EndIntent extends Intent {}
