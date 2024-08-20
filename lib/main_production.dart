e6930d80b55f6e0690718de6de16a48c50cb22f6
Herman Kravchenko <herman.kravchenko@nicholasandco.com>
2024-08-20 15:44:58 -0600

    replace with enterprise provider

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -55,7 +55,7 @@ Future<void> main() async {
       }
 
       final temp = await FirebaseAppCheck.instanceFor(app: app).activate(
-        webProvider: ReCaptchaV3Provider(kReleaseMode
+        webProvider: ReCaptchaEnterpriseProvider(kReleaseMode
             ? '6LevUCsqAAAAAEG431Qk2NsMNXurWJ8vs89UkrEG'
             : '4A104621-0F8F-4D82-A07F-008910737512'),
       );
