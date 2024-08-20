c807ea9cd21a69df39e650165cd34ffe991097ff
Herman Kravchenko <herman.kravchenko@nicholasandco.com>
2024-08-20 15:44:58 -0600

    replace with enterprise provider

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -33,7 +33,7 @@ void main() async {
   try {
     if (kIsWeb) {
       final temp = await FirebaseAppCheck.instanceFor(app: app).activate(
-        webProvider: ReCaptchaV3Provider(kReleaseMode
+        webProvider: ReCaptchaEnterpriseProvider(kReleaseMode
             ? '6LevUCsqAAAAAEG431Qk2NsMNXurWJ8vs89UkrEG'
             : '4A104621-0F8F-4D82-A07F-008910737512'),
       );
