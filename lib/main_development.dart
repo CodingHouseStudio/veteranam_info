2bc19e145a5c03db455559ef49666e2aab4417a1
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-08-20 20:21:54 +0300

    firebase app check for dev web

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:veteranam/app.dart';
@@ -10,5 +11,12 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAppCheck.instance.activate(
+    // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
+    // argument for `webProvider`
+    webProvider:
+        ReCaptchaV3Provider('6LevUCsqAAAAAMPWh00wW3HQ7_yS2RYszBKLthP9'),
+  );
+
   await bootstrap(App.new);
 }
