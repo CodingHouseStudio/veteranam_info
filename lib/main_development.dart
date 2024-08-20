20acc8a43521dd30938effd02d35b981cd757b4f
Herman Kravchenko <herman.kravchenko@nicholasandco.com>
2024-08-20 15:19:37 -0600

    hopefully fix appcheckk

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,12 @@
+import 'dart:developer';
+
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_performance/firebase_performance.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:veteranam/app.dart';
 import 'package:veteranam/bootstrap.dart';
 import 'package:veteranam/firebase_options_development.dart';
@@ -8,15 +14,66 @@ import 'package:veteranam/firebase_options_development.dart';
 /// COMMENT: DEV main file
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  final app = await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseAppCheck.instance.activate(
-    // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
-    // argument for `webProvider`
-    webProvider:
-        ReCaptchaV3Provider('6LevUCsqAAAAAMPWh00wW3HQ7_yS2RYszBKLthP9'),
-  );
+
+  try {
+    if (kIsWeb) {
+      await FirebasePerformance.instanceFor(app: app)
+          .setPerformanceCollectionEnabled(kReleaseMode);
+    } else {
+      await FirebasePerformance.instance
+          .setPerformanceCollectionEnabled(kReleaseMode);
+    }
+  } catch (e) {
+    print('firebase performance issue');
+    print(e);
+  }
+  try {
+    if (kIsWeb) {
+      final temp = await FirebaseAppCheck.instanceFor(app: app).activate(
+        webProvider: ReCaptchaV3Provider(kReleaseMode
+            ? '6LevUCsqAAAAAMPWh00wW3HQ7_yS2RYszBKLthP9'
+            : '4A104621-0F8F-4D82-A07F-008910737512'),
+      );
+    } else {
+      await FirebaseAppCheck.instance.activate(
+        webProvider: ReCaptchaV3Provider(kReleaseMode
+            ? '6LevUCsqAAAAAMPWh00wW3HQ7_yS2RYszBKLthP9'
+            : '4A104621-0F8F-4D82-A07F-008910737512'),
+      );
+    }
+  } catch (e) {
+    print('firebase app check issue');
+    print(e);
+  }
+
+  // Non-async exceptions
+  FlutterError.onError = (details) {
+    if (kIsWeb) {
+      Sentry.captureException(
+        details.exceptionAsString(),
+        stackTrace: details.stack,
+      );
+    } else {
+      FirebaseCrashlytics.instance.recordFlutterError(details);
+    }
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+  // Async exceptions
+  PlatformDispatcher.instance.onError = (error, stack) {
+    if (kIsWeb) {
+      Sentry.captureException(
+        error,
+        stackTrace: stack,
+      );
+    } else {
+      FirebaseCrashlytics.instance.recordError(error, stack);
+    }
+
+    return true;
+  };
 
   await bootstrap(App.new);
 }
