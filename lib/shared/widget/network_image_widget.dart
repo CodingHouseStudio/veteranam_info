4ebb5b5cf9a73efaf41b0d745960713eed4bd777
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-08-22 20:30:34 +0300

    use cloudflare only in release mode

--- lib/shared/widget/network_image_widget.dart
+++ lib/shared/widget/network_image_widget.dart
@@ -1,5 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:veteranam/shared/shared.dart';
@@ -104,7 +104,7 @@ class _NetworkImageWidgetState extends State<NetworkImageWidget> {
     // );
   }
 
-  String get _url => Config.isProduction
+  String get _url => Config.isProduction && kReleaseMode
       ? 'https://veteranam.info$_urlPrefix${widget.imageUrl}'
       : widget.imageUrl;
 
