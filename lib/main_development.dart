dc5b299c629cab1dbc46c9ba58a810c031b73c6e
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-08-27 00:05:33 +0300

    use firebase analytics, firebase performance, sentry, and crashlytics in Release mode only and production (#552)

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,12 +1,5 @@
-import 'dart:developer';
-
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_performance/firebase_performance.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:veteranam/app.dart';
 import 'package:veteranam/bootstrap.dart';
 import 'package:veteranam/firebase_options_development.dart';
@@ -14,70 +7,72 @@ import 'package:veteranam/firebase_options_development.dart';
 /// COMMENT: DEV main file
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final app = await Firebase.initializeApp(
+  // final app =
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   // try {
-  if (kIsWeb) {
-    await FirebasePerformance.instanceFor(app: app)
-        .setPerformanceCollectionEnabled(kReleaseMode);
-  } else {
-    await FirebasePerformance.instance
-        .setPerformanceCollectionEnabled(kReleaseMode);
-  }
+  // if (kIsWeb) {
+  //   await FirebasePerformance.instanceFor(app: app)
+  //       .setPerformanceCollectionEnabled(kReleaseMode);
+  // } else {
+  //   await FirebasePerformance.instance
+  //       .setPerformanceCollectionEnabled(kReleaseMode);
+  // }
   // } catch (e) {
   //   print('firebase performance issue');
   //   print(e);
   // }
   // try {
-  if (!kIsWeb) {
-    // {
-    //   final temp = await FirebaseAppCheck.instanceFor(app: app).activate(
-    //     webProvider: ReCaptchaV3Provider(kReleaseMode
-    //         ? 'REDACTED'
-    //         : '4A104621-0F8F-4D82-A07F-008910737512'),
-    //   );
-    // } else {
-    await FirebaseAppCheck.instance.activate(
-      androidProvider:
-          kReleaseMode ? AndroidProvider.playIntegrity : AndroidProvider.debug,
-      appleProvider: kReleaseMode
-          ? AppleProvider.appAttestWithDeviceCheckFallback
-          : AppleProvider.debug,
-    );
-  }
+  // if (!kIsWeb) {
+  //   // {
+  //   //   final temp = await FirebaseAppCheck.instanceFor(app: app).activate(
+  //   //     webProvider: ReCaptchaV3Provider(kReleaseMode
+  //   //         ? 'REDACTED'
+  //   //         : '4A104621-0F8F-4D82-A07F-008910737512'),
+  //   //   );
+  //   // } else {
+  //   await FirebaseAppCheck.instance.activate(
+  //     androidProvider:
+  //         kReleaseMode ? AndroidProvider.playIntegrity :
+  // AndroidProvider.debug,
+  //     appleProvider: kReleaseMode
+  //         ? AppleProvider.appAttestWithDeviceCheckFallback
+  //         : AppleProvider.debug,
+  //   );
+  // }
   // } catch (e) {
   //   print('firebase app check issue');
   //   print(e);
   // }
 
   // Non-async exceptions
-  FlutterError.onError = (details) {
-    if (kIsWeb) {
-      Sentry.captureException(
-        details.exceptionAsString(),
-        stackTrace: details.stack,
-      );
-    } else {
-      FirebaseCrashlytics.instance.recordFlutterError(details);
-    }
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
+  // FlutterError.onError = (details) {
+  //   if (kIsWeb) {
+  //     Sentry.captureException(
+  //       details.exceptionAsString(),
+  //       stackTrace: details.stack,
+  //     );
+  //   } else {
+  //     FirebaseCrashlytics.instance.recordFlutterError(details);
+  //   }
+  //   log(details.exceptionAsString(), stackTrace: details.stack);
+  // };
   // Async exceptions
-  PlatformDispatcher.instance.onError = (error, stack) {
-    if (kIsWeb) {
-      Sentry.captureException(
-        error,
-        stackTrace: stack,
-      );
-    } else {
-      FirebaseCrashlytics.instance.recordError(error, stack);
-    }
-    log(error.toString(), stackTrace: stack);
+  // PlatformDispatcher.instance.onError = (error, stack) {
+  //   if (kIsWeb) {
+  //     Sentry.captureException(
+  //       error,
+  //       stackTrace: stack,
+  //     );
+  //   } else {
+  //     FirebaseCrashlytics.instance.recordError(error, stack);
+  //   }
+  //   log(error.toString(), stackTrace: stack);
 
-    return true;
-  };
+  //   return true;
+  // };
 
   await bootstrap(App.new);
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -20,101 +20,103 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  if (kIsWeb) {
-    await SentryFlutter.init(
-      (options) {
-        options
-          ..dsn =
-              'REDACTED'
-          // Set tracesSampleRate to 1.0 to capture 100% of transactions for
-          // performance monitoring.
-          // We recommend adjusting this value in production.
-          ..tracesSampleRate = 1.0
-          // The sampling rate for profiling is relative to tracesSampleRate
-          // Setting to 1.0 will profile 100% of sampled transactions:
-          ..profilesSampleRate = 1.0;
-      },
-    );
-  }
-
-  // try {
-  if (kIsWeb) {
-    await FirebasePerformance.instanceFor(app: app)
-        .setPerformanceCollectionEnabled(kReleaseMode);
-
-    //   final temp = await FirebasePerformance.instanceFor(app: app)
-    //       .isPerformanceCollectionEnabled();
-    //   if (temp) {
-    //     await FirebasePerformance.instanceFor(app: app)
-    //         .newTrace('test-web')
-    //         .start();
-    //     print('Performance collection is enabled');
-
-    //     await FirebasePerformance.instanceFor(app: app)
-    //         .newTrace('test-web')
-    //         .stop();
-    //   } else {
-    //     print('Performance collection is disabled');
-    //   }
-  } else {
-    await FirebasePerformance.instance
-        .setPerformanceCollectionEnabled(kReleaseMode);
-  }
-  // } catch (e) {}
-  try {
-    if (!kIsWeb)
-    // {
-    //   if (kReleaseMode) {
-    //     await FirebaseAppCheck.instanceFor(app: app).activate(
-    //       webProvider: ReCaptchaV3Provider(
-    //         'REDACTED',
-    //       ),
-    //     );
-    //   } else {
-    //     await FirebaseAppCheck.instanceFor(app: app).activate(
-    //       webProvider: ReCaptchaV3Provider(
-    //         '326946D7-ABAE-4F1A-AEE7-395C5E23F0D4',
-    //       ),
-    //     );
-    //   }
-    // } else
-    {
-      await FirebaseAppCheck.instance.activate(
-        androidProvider: kReleaseMode
-            ? AndroidProvider.playIntegrity
-            : AndroidProvider.debug,
-        appleProvider:
-            kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
-      );
-    }
-  } catch (e) {}
-
-  // Non-async exceptions
-  FlutterError.onError = (details) {
+  if (kReleaseMode) {
     if (kIsWeb) {
-      Sentry.captureException(
-        details.exceptionAsString(),
-        stackTrace: details.stack,
+      await SentryFlutter.init(
+        (options) {
+          options
+            ..dsn =
+                'REDACTED'
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for
+            // performance monitoring.
+            // We recommend adjusting this value in production.
+            ..tracesSampleRate = 1.0
+            // The sampling rate for profiling is relative to tracesSampleRate
+            // Setting to 1.0 will profile 100% of sampled transactions:
+            ..profilesSampleRate = 1.0;
+        },
       );
-    } else {
-      FirebaseCrashlytics.instance.recordFlutterError(details);
     }
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
-  // Async exceptions
-  PlatformDispatcher.instance.onError = (error, stack) {
-    if (kIsWeb) {
-      Sentry.captureException(
-        error,
-        stackTrace: stack,
-      );
-    } else {
-      FirebaseCrashlytics.instance.recordError(error, stack);
-    }
-    log(error.toString(), stackTrace: stack);
 
-    return true;
-  };
+    // try {
+    if (kIsWeb) {
+      await FirebasePerformance.instanceFor(app: app)
+          .setPerformanceCollectionEnabled(kReleaseMode);
+
+      //   final temp = await FirebasePerformance.instanceFor(app: app)
+      //       .isPerformanceCollectionEnabled();
+      //   if (temp) {
+      //     await FirebasePerformance.instanceFor(app: app)
+      //         .newTrace('test-web')
+      //         .start();
+      //     print('Performance collection is enabled');
+
+      //     await FirebasePerformance.instanceFor(app: app)
+      //         .newTrace('test-web')
+      //         .stop();
+      //   } else {
+      //     print('Performance collection is disabled');
+      //   }
+    } else {
+      await FirebasePerformance.instance
+          .setPerformanceCollectionEnabled(kReleaseMode);
+    }
+    // } catch (e) {}
+    try {
+      if (!kIsWeb)
+      // {
+      //   if (kReleaseMode) {
+      //     await FirebaseAppCheck.instanceFor(app: app).activate(
+      //       webProvider: ReCaptchaV3Provider(
+      //         'REDACTED',
+      //       ),
+      //     );
+      //   } else {
+      //     await FirebaseAppCheck.instanceFor(app: app).activate(
+      //       webProvider: ReCaptchaV3Provider(
+      //         '326946D7-ABAE-4F1A-AEE7-395C5E23F0D4',
+      //       ),
+      //     );
+      //   }
+      // } else
+      {
+        await FirebaseAppCheck.instance.activate(
+          androidProvider: kReleaseMode
+              ? AndroidProvider.playIntegrity
+              : AndroidProvider.debug,
+          appleProvider:
+              kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
+        );
+      }
+    } catch (e) {}
+
+    // Non-async exceptions
+    FlutterError.onError = (details) {
+      if (kIsWeb) {
+        Sentry.captureException(
+          details.exceptionAsString(),
+          stackTrace: details.stack,
+        );
+      } else {
+        FirebaseCrashlytics.instance.recordFlutterError(details);
+      }
+      log(details.exceptionAsString(), stackTrace: details.stack);
+    };
+    // Async exceptions
+    PlatformDispatcher.instance.onError = (error, stack) {
+      if (kIsWeb) {
+        Sentry.captureException(
+          error,
+          stackTrace: stack,
+        );
+      } else {
+        FirebaseCrashlytics.instance.recordError(error, stack);
+      }
+      log(error.toString(), stackTrace: stack);
+
+      return true;
+    };
+  }
 
   await bootstrap(App.new);
 }
