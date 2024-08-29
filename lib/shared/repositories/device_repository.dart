a6e6c0753e2d8470b63406fcf89246fabce6f8e9
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-08-29 18:10:05 +0300

    code refactoring

--- lib/shared/repositories/device_repository.dart
+++ lib/shared/repositories/device_repository.dart
@@ -3,7 +3,7 @@ import 'dart:io' show Platform;
 import 'package:dartz/dartz.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart' show kReleaseMode;
+import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 import 'package:injectable/injectable.dart';
 import 'package:veteranam/shared/shared.dart';
 
@@ -23,16 +23,10 @@ class DeviceRepository implements IDeviceRepository {
 
   @override
   Future<Either<SomeFailure, DeviceModel?>> getDevice() async {
-    late String id;
-    late SomeFailure? failure;
-    late String? fcm;
-
-    final idResult = await getDeviceId();
-    idResult.fold(
-      (l) => failure = l,
-      (r) => id = r,
-    );
-    if (failure != null) return Left(failure!);
+    late var id = '';
+    SomeFailure? failure;
+    String? fcm;
+    final platform = PlatformEnum.getPlatform;
 
     final fcmResult = await getFcm();
     fcmResult.fold(
@@ -41,7 +35,21 @@ class DeviceRepository implements IDeviceRepository {
     );
     if (failure != null) return Left(failure!);
 
-    return Right(DeviceModel(deviceId: id, fcmToken: fcm));
+    final idResult = await getDeviceId();
+    idResult.fold(
+      (l) => failure = l,
+      (r) => id = r,
+    );
+
+    return Right(
+      DeviceModel(
+        deviceId: id,
+        fcmToken: fcm,
+        date: ExtendedDateTime.current,
+        build: '',
+        platform: PlatformEnum.android,
+      ),
+    );
     // if (kReleaseMode) {
     // if (Platform.isIOS || Platform.isAndroid) {
     //   // String? temp2;
@@ -85,20 +93,27 @@ class DeviceRepository implements IDeviceRepository {
   }
 
   @override
-  Future<Either<SomeFailure, String>> getDeviceId() async {
+  Future<Either<SomeFailure, String>> getDeviceId({
+    PlatformEnum? platformValue,
+  }) async {
     try {
+      final platform = platformValue ?? PlatformEnum.getPlatform;
       late String deviceId;
-      if (Platform.isAndroid) {
-        final dev = await _deviceInfoPlugin.androidInfo;
-        deviceId = dev.id;
-      } else if (Platform.isIOS) {
-        final dev = await _deviceInfoPlugin.iosInfo;
+      switch (platform) {
+        case PlatformEnum.android:
+          final dev = await _deviceInfoPlugin.androidInfo;
+          deviceId = dev.id;
+        case PlatformEnum.ios:
+          final dev = await _deviceInfoPlugin.iosInfo;
 
-        deviceId = dev.identifierForVendor ?? 'unknown';
-      } else {
-        final dev = await _deviceInfoPlugin.webBrowserInfo;
+          deviceId = dev.identifierForVendor ?? 'unknown';
+        case PlatformEnum.web:
+          final dev = await _deviceInfoPlugin.webBrowserInfo;
 
-        deviceId = dev.userAgent ?? dev.vendor ?? 'unknown';
+          deviceId = dev.userAgent ?? dev.vendor ?? 'unknown';
+        case PlatformEnum.unknown:
+          final dev = await _deviceInfoPlugin.deviceInfo;
+          deviceId = dev.toString();
       }
       return Right(deviceId);
     } catch (e) {
@@ -107,11 +122,30 @@ class DeviceRepository implements IDeviceRepository {
   }
 
   @override
-  Future<Either<SomeFailure, String?>> getFcm() async {
+  Future<Either<SomeFailure, String?>> getFcm({
+    PlatformEnum? platformValue,
+  }) async {
     try {
+      final platform = platformValue ?? PlatformEnum.getPlatform;
       String? temp2;
       final notificationSettings =
           await _firebaseMessaging.getNotificationSettings();
+      switch (platform) {
+        case PlatformEnum.android:
+          final dev = await _deviceInfoPlugin.androidInfo;
+          temp2 = dev.id;
+        case PlatformEnum.ios:
+          final dev = await _deviceInfoPlugin.iosInfo;
+
+          temp2 = dev.identifierForVendor ?? 'unknown';
+        case PlatformEnum.web:
+          final dev = await _deviceInfoPlugin.webBrowserInfo;
+
+          temp2 = dev.userAgent ?? dev.vendor ?? 'unknown';
+        case PlatformEnum.unknown:
+          final dev = await _deviceInfoPlugin.deviceInfo;
+          temp2 = dev.toString();
+      }
 
       /// Do we need this?
 
@@ -125,13 +159,13 @@ class DeviceRepository implements IDeviceRepository {
       // For apple platforms, ensure the APNS token is available before making
       // any FCM plugin API calls
       final apnsToken = await _firebaseMessaging.getAPNSToken();
-      if (Platform.isIOS) {
-        FirebaseMessaging.instance.requestPermission(provisional: true);
+      if (!kIsWeb && Platform.isIOS) {
+        await FirebaseMessaging.instance.requestPermission(provisional: true);
       }
 
       if (notificationSettings.authorizationStatus ==
               AuthorizationStatus.authorized &&
-          (!Platform.isIOS || apnsToken != null)) {
+          (kIsWeb || !Platform.isIOS || apnsToken != null)) {
         temp2 = await _firebaseMessaging.getToken(
           vapidKey: Config.isProduction
               ? KSecurityKeys.firebaseProdVapidKey
