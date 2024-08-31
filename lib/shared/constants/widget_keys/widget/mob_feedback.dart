4df0393be7024fd5128a60cf1d6b4520ad4f8d44
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-08-31 03:35:04 +0300

    mobile tool tip change (#565)

--- lib/shared/constants/widget_keys/widget/mob_feedback.dart
+++ lib/shared/constants/widget_keys/widget/mob_feedback.dart
@@ -2,8 +2,8 @@ part of '../widget_keys.dart';
 
 class _MobFeedbackKeys {
   final widget = const Key('mob_feedback');
-  final text = const Key('mob_feedback_text');
+  final title = const Key('mob_feedback_title');
   final field = const Key('mob_feedback_field');
-  final icon = const Key('mob_feedback_icon');
+  // final icon = const Key('mob_feedback_icon');
   final button = const Key('mob_feedback_button');
 }
