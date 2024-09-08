aebe4089ee730e0229e2ce4c291fdc76d6c1afc1
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-09-08 10:37:25 +0300

    prod firebase app check

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -80,31 +80,36 @@ Future<void> main() async {
     }
     // } catch (e, stack) {}
     try {
-      if (!kIsWeb)
+      // if (!kIsWeb)
       // {
-      //   if (kReleaseMode) {
-      //     await FirebaseAppCheck.instanceFor(app: app).activate(
-      //       webProvider: ReCaptchaV3Provider(
-      //         '6LevUCsqAAAAAEG431Qk2NsMNXurWJ8vs89UkrEG',
-      //       ),
-      //     );
-      //   } else {
-      //     await FirebaseAppCheck.instanceFor(app: app).activate(
-      //       webProvider: ReCaptchaV3Provider(
-      //         '326946D7-ABAE-4F1A-AEE7-395C5E23F0D4',
-      //       ),
-      //     );
-      //   }
+      // if (kReleaseMode) {
+      await FirebaseAppCheck.instanceFor(app: app).activate(
+        webProvider: ReCaptchaV3Provider(
+          '6LcHRzkqAAAAACJhOV9aPEb3tcA8wAzS-a5Qww_J',
+        ),
+      );
+      await FirebaseAppCheck.instance.activate(
+        webProvider: ReCaptchaV3Provider(
+          '6LcHRzkqAAAAACJhOV9aPEb3tcA8wAzS-a5Qww_J',
+        ),
+      );
+      // } else {
+      //   await FirebaseAppCheck.instanceFor(app: app).activate(
+      //     webProvider: ReCaptchaV3Provider(
+      //       '326946D7-ABAE-4F1A-AEE7-395C5E23F0D4',
+      //     ),
+      //   );
+      // }
       // } else
-      {
-        await FirebaseAppCheck.instance.activate(
-          androidProvider: kReleaseMode
-              ? AndroidProvider.playIntegrity
-              : AndroidProvider.debug,
-          appleProvider:
-              kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
-        );
-      }
+      // {
+      //   await FirebaseAppCheck.instance.activate(
+      //     androidProvider: kReleaseMode
+      //         ? AndroidProvider.playIntegrity
+      //         : AndroidProvider.debug,
+      //     appleProvider:
+      //         kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
+      //   );
+      // }
     } catch (e) {}
 
     // Non-async exceptions
