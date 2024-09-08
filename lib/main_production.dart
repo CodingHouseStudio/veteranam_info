7e8f799745f85a12294b9cdb6f775b7ebf0302d4
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-09-08 10:38:09 +0300

    prod firebase app check (#627)

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -80,31 +80,36 @@ Future<void> main() async {
     }
     // } catch (e, stack) {}
     try {
-      if (!kIsWeb)
+      // if (!kIsWeb)
       // {
-      //   if (kReleaseMode) {
-      //     await FirebaseAppCheck.instanceFor(app: app).activate(
-      //       webProvider: ReCaptchaV3Provider(
-      //         'REDACTED',
-      //       ),
-      //     );
-      //   } else {
-      //     await FirebaseAppCheck.instanceFor(app: app).activate(
-      //       webProvider: ReCaptchaV3Provider(
-      //         '326946D7-ABAE-4F1A-AEE7-395C5E23F0D4',
-      //       ),
-      //     );
-      //   }
+      // if (kReleaseMode) {
+      await FirebaseAppCheck.instanceFor(app: app).activate(
+        webProvider: ReCaptchaV3Provider(
+          'REDACTED',
+        ),
+      );
+      await FirebaseAppCheck.instance.activate(
+        webProvider: ReCaptchaV3Provider(
+          'REDACTED',
+        ),
+      );
+      // } else {
+      //   await FirebaseAppCheck.instanceFor(app: app).activate(
+      //     webProvider: ReCaptchaV3Provider(
+      //       '326946D7-ABAE-4F1A-AEE7-395C5E23F0D4',
+      //     ),
+      //   );
+      // }
       // } else
-      {
-        await FirebaseAppCheck.instance.activate(
-          androidProvider: kReleaseMode
-              ? AndroidProvider.playIntegrity
-              : AndroidProvider.debug,
-          appleProvider:
-              kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
-        );
-      }
+      // {
+      //   await FirebaseAppCheck.instance.activate(
+      //     androidProvider: kReleaseMode
+      //         ? AndroidProvider.playIntegrity
+      //         : AndroidProvider.debug,
+      //     appleProvider:
+      //         kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
+      //   );
+      // }
     } catch (e) {}
 
     // Non-async exceptions
