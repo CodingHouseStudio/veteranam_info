cdd489b102a9818b9d34b74b8144203d94ba7f24
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-10-10 15:07:51 +0300

    ios fix status bar color (#695)

--- lib/shared/constants/extension/field_models_error_get.dart
+++ lib/shared/constants/extension/field_models_error_get.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' show BuildContext;
 import 'package:veteranam/shared/shared.dart';
 
 extension EmailFieldModelValidationErrorEmpl
--- lib/shared/constants/style/text_styles.dart
+++ lib/shared/constants/style/text_styles.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show FontStyle, FontWeight, TextDecoration, TextStyle;
 import 'package:veteranam/shared/shared.dart';
 
 /// Text Styles
