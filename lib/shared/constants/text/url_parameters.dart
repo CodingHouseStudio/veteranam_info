2ca3d37ccec4a0b772fe18dbf848aebf07bf78c1
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-10-11 17:15:05 +0300

    fix: password forgot flow (#701)

--- lib/shared/constants/text/url_parameters.dart
+++ lib/shared/constants/text/url_parameters.dart
@@ -1,5 +1,5 @@
 abstract class UrlParameters {
   static const email = 'image';
-  static const verificationCode = 'png';
+  static const verificationCode = 'oobCode';
   static const cardId = 'card_id';
 }
