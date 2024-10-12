0b62ab4224a8a1b8cc2af401dc3b240af7629869
Mariia Melnychenko <133329717+MariiaMelnychenko@users.noreply.github.com>
2024-10-12 10:48:13 +0300

    feat: dialogs with new design (#702)
    * dialogs with new design
    
    * fix

---
+++ test/my_discounts/helper/my_discount_confirm_button_helper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../test_dependency.dart';
+import 'helper.dart';
+
+Future<void> myDiscountConfirmButtonlHelper({
+  required WidgetTester tester,
+  required MockGoRouter mockGoRouter,
+}) async {
+  await myDiscountDialogHelper(tester);
+
+  await dialogConfirmHelper(
+    tester: tester,
+    mockGoRouter: mockGoRouter,
+  );
+}
