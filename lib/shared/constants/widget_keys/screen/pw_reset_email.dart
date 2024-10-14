c0ef2fcdc235a5499dc45772cbdc315e468f9e56
Mariia Melnychenko <133329717+MariiaMelnychenko@users.noreply.github.com>
2024-10-14 18:18:54 +0300

    feat: back button (#716)
    * back button
    
    * fix

--- lib/shared/constants/widget_keys/screen/pw_reset_email.dart
+++ lib/shared/constants/widget_keys/screen/pw_reset_email.dart
@@ -7,6 +7,7 @@ class _PwResetEmailKeys {
   final emailField = const Key('pw_reset_email_email_field');
   final sendButton = const Key('pw_reset_email_send_button');
   final submitingText = const Key('pw_reset_email_submiting_text');
+  final cancelButton = const Key('pw_reset_email_cancel_button');
   final delayText = const Key('pw_reset_email_delay_text');
   final resendText = const Key('pw_reset_email_resend_text');
   final resendButton = const Key('pw_reset_email_resend_button');
