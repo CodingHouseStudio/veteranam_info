d495f64eb60e8ee62f518f3ffd22366b68bf015a
Ferrera Marria <marria.ferrera74@gmail.com>
2024-10-15 17:38:53 +0300

    possible to log out and hide delete button

--- lib/components/profile/widget/body/profile_body_widget.dart
+++ lib/components/profile/widget/body/profile_body_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:veteranam/components/profile/profile.dart';
 import 'package:veteranam/shared/shared.dart';
 
@@ -77,27 +76,31 @@ class ProfileBodyWidget extends StatelessWidget {
             else
               KSizedBox.kHeightSizedBox48,
             if (isDesk)
-              Row(
-                children: [
-                  Expanded(
-                    child: logoutButton(
-                      context: context,
-                      isDesk: isDesk,
-                    ),
-                  ),
-                  KSizedBox.kWidthSizedBox40,
-                  Expanded(
-                    child: deleteButton(
-                      context: context,
-                      isDesk: isDesk,
-                    ),
-                  ),
-                ],
+              logoutButton(
+                context: context,
+                isDesk: false,
               )
+            // Row(
+            //   children: [
+            //     Expanded(
+            //       child: logoutButton(
+            //         context: context,
+            //         isDesk: isDesk,
+            //       ),
+            //     ),
+            //     KSizedBox.kWidthSizedBox40,
+            //     Expanded(
+            //       child: deleteButton(
+            //         context: context,
+            //         isDesk: isDesk,
+            //       ),
+            //     ),
+            //   ],
+            // )
             else ...[
               logoutButton(context: context, isDesk: isDesk),
-              KSizedBox.kHeightSizedBox16,
-              deleteButton(context: context, isDesk: isDesk),
+              // KSizedBox.kHeightSizedBox16,
+              // deleteButton(context: context, isDesk: isDesk),
             ],
             if (isDesk)
               KSizedBox.kHeightSizedBox48
@@ -126,9 +129,8 @@ class ProfileBodyWidget extends StatelessWidget {
           background: AppColors.materialThemeKeyColorsSecondary,
           onPressed: () {
             context.read<AuthenticationBloc>().add(
-                  AuthenticationDeleteRequested(),
+                  AuthenticationLogoutRequested(),
                 );
-            context.goNamed(KRoute.home.name);
           },
         ),
         isDesk: isDesk,
@@ -167,9 +169,8 @@ class ProfileBodyWidget extends StatelessWidget {
           background: AppColors.materialThemeRefErrorError60,
           onPressed: () {
             context.read<AuthenticationBloc>().add(
-                  AuthenticationLogoutRequested(),
+                  AuthenticationDeleteRequested(),
                 );
-            context.goNamed(KRoute.home.name);
           },
         ),
       );
