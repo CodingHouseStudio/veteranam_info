130208c086351f9cf22e1e9422fc02fc84532330
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-10-21 19:06:45 +0300

    test: Company model tests (#740)
    * company tests
    
    * fix card id

---
+++ test/bloc/company/company_model_test.dart
@@ -0,0 +1,135 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:veteranam/shared/shared.dart';
+
+import '../../test_dependency.dart';
+
+void main() {
+  setUp(configureFailureDependenciesTest);
+
+  tearDown(GetIt.I.reset);
+  group('${KScreenBlocName.company} ${KGroupText.model}', () {
+    test('check is Empty ', () {
+      expect(
+        KTestText.fullCompanyModel.isEmpty,
+        isFalse,
+      );
+
+      expect(
+        KTestText.pureCompanyModel.isEmpty,
+        isTrue,
+      );
+    });
+    test('check is not Empty ', () {
+      expect(
+        KTestText.fullCompanyModel.isNotEmpty,
+        isTrue,
+      );
+
+      expect(
+        KTestText.pureCompanyModel.isNotEmpty,
+        isFalse,
+      );
+    });
+    final fullJson = {
+      CompanyModelJsonField.id: KTestText.fullCompanyModel.id,
+      CompanyModelJsonField.companyName: KTestText.fullCompanyModel.companyName,
+      CompanyModelJsonField.publicName: KTestText.fullCompanyModel.publicName,
+      CompanyModelJsonField.link: KTestText.fullCompanyModel.link,
+      CompanyModelJsonField.code: KTestText.fullCompanyModel.code,
+      CompanyModelJsonField.image: [KTestText.imageModel.toJson()],
+      CompanyModelJsonField.userEmails: KTestText.fullCompanyModel.userEmails,
+    };
+    final nullableJson = {
+      CompanyModelJsonField.id: KTestText.fullCompanyModel.id,
+      CompanyModelJsonField.companyName: null,
+      CompanyModelJsonField.publicName: null,
+      CompanyModelJsonField.link: null,
+      CompanyModelJsonField.image: null,
+      CompanyModelJsonField.code: null,
+      CompanyModelJsonField.userEmails: KTestText.fullCompanyModel.userEmails,
+    };
+    group('${KGroupText.modelJson} ', () {
+      test('${KGroupText.full} ', () {
+        final fullCompanyModel = CompanyModel.fromJson(fullJson);
+
+        expect(fullCompanyModel.id, KTestText.fullCompanyModel.id);
+        expect(
+          fullCompanyModel.userEmails,
+          KTestText.fullCompanyModel.userEmails,
+        );
+        expect(
+          fullCompanyModel.companyName,
+          KTestText.fullCompanyModel.companyName,
+        );
+        expect(
+          fullCompanyModel.code,
+          KTestText.fullCompanyModel.code,
+        );
+        expect(
+          fullCompanyModel.publicName,
+          KTestText.fullCompanyModel.publicName,
+        );
+        expect(fullCompanyModel.link, KTestText.fullCompanyModel.link);
+        expect(fullCompanyModel.image, KTestText.imageModel);
+      });
+      test('${KGroupText.nullable} ', () {
+        final fullCompanyModel = CompanyModel.fromJson(nullableJson);
+
+        expect(fullCompanyModel.id, KTestText.fullCompanyModel.id);
+        expect(
+          fullCompanyModel.userEmails,
+          KTestText.fullCompanyModel.userEmails,
+        );
+        expect(
+          null,
+          isNull,
+        );
+        expect(fullCompanyModel.companyName, isNull);
+        expect(fullCompanyModel.publicName, isNull);
+        expect(fullCompanyModel.image, isNull);
+        expect(fullCompanyModel.link, isNull);
+        expect(fullCompanyModel.image, isNull);
+        expect(fullCompanyModel.code, isNull);
+      });
+
+      test('${KGroupText.failure} ', () {
+        final json = {
+          // id is missing
+          CompanyModelJsonField.companyName:
+              KTestText.fullCompanyModel.companyName,
+          CompanyModelJsonField.publicName:
+              KTestText.fullCompanyModel.publicName,
+          CompanyModelJsonField.link: KTestText.fullCompanyModel.link,
+          CompanyModelJsonField.image: KTestText.fullCompanyModel.image,
+          CompanyModelJsonField.userEmails:
+              KTestText.fullCompanyModel.userEmails,
+          CompanyModelJsonField.code: KTestText.fullCompanyModel.code,
+        };
+
+        expect(
+          () => CompanyModel.fromJson(json),
+          throwsA(isA<TypeError>()),
+        );
+      });
+    });
+    group('${KGroupText.jsonModel} ', () {
+      test('${KGroupText.full} ', () {
+        final fullCompanyModelModelJson = KTestText.fullCompanyModel
+            .copyWith(image: KTestText.imageModel)
+            .toJson();
+
+        expect(fullCompanyModelModelJson, fullJson);
+      });
+
+      test('${KGroupText.nullable} ', () {
+        final fullCompanyModelModelJson = CompanyModel(
+          id: KTestText.fullCompanyModel.id,
+          userEmails: KTestText.fullCompanyModel.userEmails,
+        ).toJson();
+
+        expect(fullCompanyModelModelJson, nullableJson);
+      });
+    });
+  });
+}
