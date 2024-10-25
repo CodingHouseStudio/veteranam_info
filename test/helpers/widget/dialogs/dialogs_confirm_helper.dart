e66fd6d8e491e204e75cf8067894d99d30281da3
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-10-25 17:36:32 +0300

    test: authetication repository (#750)
    * authetication repository
    
    * coverage 100%
    
    * fix

--- test/helpers/widget/dialogs/dialogs_confirm_helper.dart
+++ test/helpers/widget/dialogs/dialogs_confirm_helper.dart
@@ -1,25 +1,41 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:veteranam/shared/shared.dart';
 
+import '../../../test_dependency.dart';
+
 Future<void> dialogConfirmChangesHelper(
   WidgetTester tester,
 ) async {
-  expect(find.byKey(KWidgetkeys.widget.confirmDialog.icon), findsOneWidget);
+  await changeWindowSizeHelper(
+    tester: tester,
+    scrollUp: false,
+    windowsTest: true,
+    size: KTestConstants.windowVerySmallSize,
+    test: () async {
+      expect(
+        find.byKey(KWidgetkeys.widget.confirmDialog.icon),
+        findsOneWidget,
+      );
 
-  expect(find.byKey(KWidgetkeys.widget.confirmDialog.title), findsOneWidget);
+      expect(
+        find.byKey(KWidgetkeys.widget.confirmDialog.title),
+        findsOneWidget,
+      );
 
-  expect(
-    find.byKey(KWidgetkeys.widget.confirmDialog.subtitle),
-    findsOneWidget,
-  );
+      expect(
+        find.byKey(KWidgetkeys.widget.confirmDialog.subtitle),
+        findsOneWidget,
+      );
 
-  expect(
-    find.byKey(KWidgetkeys.widget.confirmDialog.confirmButton),
-    findsOneWidget,
-  );
+      expect(
+        find.byKey(KWidgetkeys.widget.confirmDialog.confirmButton),
+        findsOneWidget,
+      );
 
-  expect(
-    find.byKey(KWidgetkeys.widget.confirmDialog.unconfirmButton),
-    findsOneWidget,
+      expect(
+        find.byKey(KWidgetkeys.widget.confirmDialog.unconfirmButton),
+        findsOneWidget,
+      );
+    },
   );
 }
