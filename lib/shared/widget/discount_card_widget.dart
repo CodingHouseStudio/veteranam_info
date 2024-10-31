0cfce7c040b3d504c6de4adb4951ad0af75c71ed
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-10-31 22:29:44 +0200

    feat: Discount card list improve performence (#761)
    * discount card list improve performence
    
    * card use id

--- lib/shared/widget/discount_card_widget.dart
+++ lib/shared/widget/discount_card_widget.dart
@@ -28,6 +28,7 @@ class DiscountCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
+      key: ValueKey(discountItem.id),
       decoration: KWidgetTheme.boxDecorationDiscountContainer,
       child: Padding(
         padding: const EdgeInsets.only(
