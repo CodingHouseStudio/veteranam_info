bfcbbcfd8c450577acf28fb0dd473ca0fcc34fc8
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2024-11-02 16:47:54 +0200

    feat: coverage excludes use txt file (#769)
    * coverage excludes use txt file
    
    * coverage excludes use txt file
    
    * CI add read coverage step
    
    * CI add read coverage step
    
    * fix
    
    * fix
    
    * all coverage excludes script use coverage_excludes.txt
    
    * code refactoring

--- coeverage_exclude_python.py
+++ coeverage_exclude_python.py
@@ -1,48 +1,28 @@
 import re
 import shutil
 
-exclude_patterns = [
-    r'lib/firebase_options_development.dart',
-    r'lib/l10n/l10n.dart',
-    r'lib/bootstrap.dart',
-    r'lib/shared/helper/get_it_service_locator.dart',
-    r'lib/shared/constants/theme_constant.dart',
-    r'lib/shared/constants/variables_constant.dart',
-    r'lib/shared/constants/.*.dart',
-    r'.*.g.dart',
-    r'.*.freezed.dart',
-    r'lib/shared/navigation/.*',
-    r'lib/shared/data_provider/.*',
-    r'.*/blocprovider/.*.dart',
-    r'lib/shared/widget/card_add_image_widget.dart',
-    r'lib/shared/widget/image_widget.dart',
-    r'lib/shared/widget/user_photo_widget.dart',
-    r'lib/shared/bloc/authentication/authentication_state.dart',
-    r'lib/shared/bloc/authentication/listenable.dart',
-    r'lib/shared/extension/.*.dart',
-    r'lib/shared/repositories/url_repository.dart',
-    r'lib/shared/bloc/authentication/authentication_event.dart',
-    r'lib/shared/repositories/firebase_module.dart',
-    r'lib/shared/repositories/network_module.dart',
-    r'lib/shared/models/field_models/field_models_error_get.dart',
-    r'lib/shared/widget/dilog_page_widget.dart',
-    r'lib/shared/widget/keyboard_scroll_view.dart',
-    r'lib/shared/widget/network_image_widget.dart',
-    r'lib/app.dart',
-    r'lib/components/story_add/widget/field_widget.dart',
-    r'lib/shared/models/failure_model/exceptions.dart',
-    r'lib/shared/widget/info_version_widget.dart',
-    r'lib/shared/widget/viewpager_indicator_widget.dart',
-    r'lib/shared/repositories/messaging_module.dart',
-    r'lib/shared/repositories/build_repository.dart',
-    r'lib/shared/widget/row_sliver_widget.dart',
-    r'lib/shared/repositories/failure_repository.dart',
-    r'lib/shared/repositories/failure_module.dart',
-    r'lib/shared/repositories/app_info_repository.dart',
-    r'lib/shared/repositories/data_picker_module.dart',
-    r'lib/shared/repositories/mobile_rating_module.dart',
-    r'lib/shared/widget/popup_menu_item_widget.dart'
-]
+def load_exclude_patterns(exclude_file):
+    """ Load and adjust exclusion patterns from the specified file. """
+    adjusted_patterns = []
+    with open(exclude_file, 'r') as file:
+        for line in file:
+            pattern = line.strip()
+            if pattern:
+                # Adjust the patterns
+                pattern = pattern.replace('**/*', '*')  # Replace '**' with '.*'
+                pattern = pattern.replace('*', '.*')   # Replace '*' with '.*' for directories
+                
+                # Add the adjusted pattern to the list if it is valid
+                try:
+                    re.compile(pattern)  # Validate the regex
+                    adjusted_patterns.append(pattern)
+                except re.error as e:
+                    print(f"Invalid pattern '{pattern}': {e}")
+
+    return adjusted_patterns
+
+exclude_file = 'coverage_excludes.txt'
+exclude_patterns = load_exclude_patterns(exclude_file)
 
 def exclude_files(input_file, temp_file, exclude_patterns):
     with open(input_file, 'r') as file:
@@ -50,18 +30,18 @@ def exclude_files(input_file, temp_file, exclude_patterns):
     with open(temp_file, 'w') as file:
         skip_record = False
         for line in lines:
-        
             if line.startswith('SF:'):
-                path = line[3:].replace("\\",'/').strip()
-            
+                path = line[3:].replace("\\", '/').strip()
+                # Check against adjusted patterns
                 if any(re.fullmatch(pattern, path) for pattern in exclude_patterns):
                     skip_record = True
-        
+            
             if skip_record:
                 if line.startswith('end_of_record'):
                     skip_record = False
             else:
                 file.write(line)
+
 if __name__ == "__main__":
     input_filename = 'coverage/lcov.info' 
     temp_filename = 'coverage/lcov_filtered.tmp'
@@ -69,4 +49,4 @@ if __name__ == "__main__":
     exclude_files(input_filename, temp_filename, exclude_patterns)
 
     shutil.move(temp_filename, input_filename)
-    print(f"Original coverage file has been replaced with the filtered version.")
+    print("Original coverage file has been replaced with the filtered version.")
--- tests_quick.sh
+++ tests_quick.sh
@@ -6,47 +6,12 @@ very_good test --concurrency=$(sysctl -n hw.logicalcpu) --recursive --optimizati
 # Generate the initial HTML coverage report
 genhtml coverage/lcov.info -o coverage/html
 
+# Define the exclusion file
+EXCLUDE_FILE='coverage_excludes.txt'
+
 # Exclude specific files and directories from the coverage report
 lcov --remove coverage/lcov.info \
-    'lib/firebase_options_development.dart' \
-    'lib/l10n/l10n.dart' \
-    'lib/bootstrap.dart' \
-    'lib/shared/helper/get_it_service_locator.dart' \
-    'lib/shared/constants/theme_constant.dart' \
-    'lib/shared/constants/variables_constant.dart' \
-    'lib/shared/constants/*' \
-    '*.g.dart' \
-    '*.freezed.dart' \
-    'lib/shared/navigation/*' \
-    'lib/shared/data_provider/*' \
-    '*/blocprovider/*.dart' \
-    'lib/shared/widget/card_add_image_widget.dart' \
-    'lib/shared/widget/image_widget.dart' \
-    'lib/shared/widget/user_photo_widget.dart' \
-    'lib/shared/bloc/authentication/authentication_state.dart' \
-    'lib/shared/bloc/authentication/listenable.dart' \
-    'lib/shared/extension/.*.dart' \
-    'lib/shared/repositories/url_repository.dart' \
-    'lib/shared/bloc/authentication/authentication_event.dart' \
-    'lib/shared/repositories/firebase_module.dart' \
-    'lib/shared/repositories/network_module.dart' \
-    'lib/shared/models/field_models/field_models_error_get.dart' \
-    'lib/shared/widget/dilog_page_widget.dart' \
-    'lib/shared/widget/keyboard_scroll_view.dart' \
-    'lib/shared/widget/network_image_widget.dart' \
-    'lib/app.dart' \
-    'lib/components/story_add/widget/field_widget.dart' \
-    'lib/shared/models/failure_model/exceptions.dart' \
-    'lib/shared/widget/info_version_widget.dart' \
-    'lib/shared/widget/viewpager_indicator_widget.dart' \
-    'lib/shared/widget/row_sliver_widget.dart' \
-    'lib/shared/repositories/messaging_module.dart' \
-    'lib/shared/repositories/build_repository.dart' \
-    'lib/shared/repositories/failure_repository.dart' \
-    'lib/shared/repositories/failure_module.dart' \
-    'lib/shared/widget/popup_menu_item_widget.dart' \
-    'lib/shared/repositories/data_picker_module.dart' \
-    'lib/shared/repositories/mobile_rating_module.dart' \
+     -f "@$EXCLUDE_FILE" \
     --output-file coverage/lcov.info
 
 # Generate the final HTML coverage report after exclusions
