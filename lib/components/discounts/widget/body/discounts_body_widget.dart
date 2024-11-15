1b00944351ce3e157df193a453565ff1532ea2e8
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-11-15 23:53:40 +0200

    code refactoring

--- lib/components/discounts/widget/body/discounts_body_widget.dart
+++ lib/components/discounts/widget/body/discounts_body_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:veteranam/components/discounts/bloc/bloc.dart';
-import 'package:veteranam/components/discounts/bloc/watcher/discount_watcher_bloc.dart';
 import 'package:veteranam/components/discounts/discounts.dart';
 import 'package:veteranam/shared/shared_flutter.dart';
 
