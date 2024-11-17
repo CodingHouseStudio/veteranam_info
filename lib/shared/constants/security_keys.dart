1bc40b3329afa145cd5eccc36177836bd07c559f
Herman Kravchenko <49967581+herman-the-worm@users.noreply.github.com>
2024-11-17 09:36:10 -0700

    migrate to environment agnostic env variables

--- lib/shared/constants/security_keys.dart
+++ lib/shared/constants/security_keys.dart
@@ -1,12 +1,6 @@
 abstract class KSecurityKeys {
-  static const appStoreId = String.fromEnvironment('APP_STORE_ID');
-  static const firebaseProdVapidKey =
-      String.fromEnvironment('FIREBASE_VAPID_PROD_KEY');
-  static const firebaseDevVapidKey =
-      String.fromEnvironment('FIREBASE_VAPID_DEV_KEY');
+  static const appStoreId = '6584519009';
+  static const firebaseVapidKey = String.fromEnvironment('FIREBASE_VAPID_KEY');
   static const sentryDSN = String.fromEnvironment('SENTRY_DSN');
-  static const firebaseAppCheckProd =
-      String.fromEnvironment('FIREBASE_APP_CHECK_PROD');
-  static const firebaseAppCheckDev =
-      String.fromEnvironment('FIREBASE_APP_CHECK_DEV');
+  static const firebaseAppCheck = String.fromEnvironment('FIREBASE_APP_CHECK');
 }
