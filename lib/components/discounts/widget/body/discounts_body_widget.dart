b84fd6fbadadf461838926f8bb77b25af6c28656
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-11-17 16:24:25 +0200

    discount page user custom scroll view again

--- lib/components/discounts/widget/body/discounts_body_widget.dart
+++ lib/components/discounts/widget/body/discounts_body_widget.dart
@@ -24,135 +24,251 @@ class _DiscountsBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final isDesk =
-            constraints.maxWidth > KPlatformConstants.minWidthThresholdDesk;
-        final isTablet =
-            constraints.maxWidth > KPlatformConstants.minWidthThresholdTablet;
-        final padding = EdgeInsets.symmetric(
-          horizontal: (isDesk
-              ? KPadding.kPaddingSize90 +
-                  ((constraints.maxWidth >
-                          KPlatformConstants.maxWidthThresholdTablet)
-                      ? (constraints.maxWidth -
-                              KPlatformConstants.maxWidthThresholdTablet) /
-                          2
-                      : 0)
-              : KPadding.kPaddingSize16),
-        );
-        final body = [
-          NavbarWidget(isDesk: isDesk, isTablet: isTablet),
-          KSizedBox.kHeightSizedBox24,
-          if (Config.isWeb) ...[
-            Padding(
-              padding: padding,
-              child: TitlePointWidget(
-                title: context.l10n.specialOffers,
-                titleKey: KWidgetkeys.screen.discounts.title,
-                titleSecondPart: context.l10n.forVeteransAndTheirFamilies,
-                // pointText: context.l10n.discounts,
-                // pointKey: KWidgetkeys.screen.discounts.
-                // titlePoint,
-                isDesk: isDesk,
-                titleSecondPartPadding: const EdgeInsets.only(
-                  left: KPadding.kPaddingSize72,
-                ),
-                iconCrossAxisAlignment: CrossAxisAlignment.end,
-                isRightArrow: false,
-              ),
-            ),
-            if (isDesk)
-              KSizedBox.kHeightSizedBox40
-            else
-              KSizedBox.kHeightSizedBox24,
-          ],
-          Padding(
-            padding: padding,
-            child: DiscountsFilterWidget(isDesk: isDesk),
-          ),
-          if (isDesk)
-            KSizedBox.kHeightSizedBox40
-          else
-            KSizedBox.kHeightSizedBox24,
-          Padding(
-            padding: padding,
-            child: DiscountsWidgetList(
-              isDesk: isDesk,
-            ),
-          ),
-          if (isDesk)
-            KSizedBox.kHeightSizedBox40
-          else
-            KSizedBox.kHeightSizedBox24,
-        ];
-        return BlocListener<DiscountWatcherBloc, DiscountWatcherState>(
-          listener: (context, state) {
-            if (state.failure != null) {
-              context.dialog.showGetErrorDialog(
-                error: state.failure!.value(context),
-                onPressed: () {},
-                // I think this event is not necessary for Stream, but
-                // I think it's better to give
-                // the user imaginary control over it
-
-                // () => context
-                //     .read<DiscountWatcherBloc>()
-                //     .add(const DiscountWatcherEvent.started()),
+    return BlocListener<NetworkCubit, NetworkStatus>(
+      listener: (context, state) {
+        if (state == NetworkStatus.network) {
+          context.read<DiscountWatcherBloc>().add(
+                DiscountWatcherEvent.started(isEnglish: context.isEnglish),
               );
-            }
-            if (context.read<UserEmailFormBloc>().state.emailEnum.show &&
-                state.itemsLoaded ==
-                    (context.read<DiscountConfigCubit>().state.loadingItems *
-                        (context
-                                .read<DiscountConfigCubit>()
-                                .state
-                                .emailScrollCount +
-                            1))) {
-              if (Config.isWeb) {
-                context.dialog.showUserEmailDialog(
-                  context.read<DiscountConfigCubit>().state.emailCloseDelay,
-                );
-              } else {
-                context.read<MobileRatingCubit>().showDialog();
-              }
-            }
-          },
-          child: BlocListener<NetworkCubit, NetworkStatus>(
-            listener: (context, state) {
-              if (state == NetworkStatus.network) {
-                context.read<DiscountWatcherBloc>().add(
-                      DiscountWatcherEvent.started(
-                        isEnglish: context.isEnglish,
-                      ),
-                    );
-              }
-            },
-            child: BlocListener<UrlCubit, UrlEnum?>(
-              listener: (context, state) async {
-                if (state != null) {
-                  context.dialog.showSnackBardTextDialog(
-                    state.value(context),
-                    duration: const Duration(milliseconds: 4000),
-                  );
-                  context.read<UrlCubit>().reset();
-                }
-              },
-              child: ListView.builder(
-                key: KWidgetkeys.widget.scaffold.scroll,
-                // primary: true,
-                controller: scrollController,
-                restorationId: 'discount_page',
-                itemCount: body.length,
-                addAutomaticKeepAlives: false,
-                addRepaintBoundaries: false,
-                itemBuilder: (context, index) => body.elementAt(index),
+        }
+      },
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final isDesk =
+              constraints.maxWidth > KPlatformConstants.minWidthThresholdDesk;
+          final isTablet =
+              constraints.maxWidth > KPlatformConstants.minWidthThresholdTablet;
+
+          final padding = EdgeInsets.symmetric(
+            horizontal: (isDesk
+                ? KPadding.kPaddingSize90 +
+                    ((constraints.maxWidth >
+                            KPlatformConstants.maxWidthThresholdTablet)
+                        ? (constraints.maxWidth -
+                                KPlatformConstants.maxWidthThresholdTablet) /
+                            2
+                        : 0)
+                : KPadding.kPaddingSize16),
+          );
+          final scaffold = FocusTraversalGroup(
+            child: Semantics(
+              child: Stack(
+                children: [
+                  Scaffold(
+                    resizeToAvoidBottomInset: true,
+                    bottomNavigationBar: Config.isWeb
+                        ? null
+                        : const MobNavigationWidget(
+                            index: 0,
+                          ),
+                    appBar: AppBar(
+                      backgroundColor: AppColors.materialThemeWhite,
+                      toolbarHeight: KSize.kAppBarHeight,
+                    ),
+                    body: CustomScrollView(
+                      key: KWidgetkeys.widget.scaffold.scroll,
+                      slivers: [
+                        if (!Config.isWeb)
+                          BlocBuilder<NetworkCubit, NetworkStatus>(
+                            builder: (context, state) {
+                              if (state.isOffline) {
+                                SliverPersistentHeader(
+                                  pinned: true,
+                                  delegate: NetworkStatusBanner.getSliverHeader(
+                                    isDesk: isDesk,
+                                    isTablet: isTablet,
+                                    networkStatus: state,
+                                  ),
+                                );
+                              }
+                              return const SliverToBoxAdapter();
+                            },
+                          ),
+                        if (Config.isWeb)
+                          SliverPersistentHeader(
+                            delegate: NavbarWidget.getSliverHeader(
+                              isDesk: isDesk,
+                              isTablet: isTablet,
+                              pageName: context.l10n.discount,
+                            ),
+                          ),
+                        SliverPadding(
+                          padding: padding,
+                          sliver: SliverToBoxAdapter(
+                            child: TitlePointWidget(
+                              title: context.l10n.specialOffers,
+                              titleKey: KWidgetkeys.screen.discounts.title,
+                              titleSecondPart:
+                                  context.l10n.forVeteransAndTheirFamilies,
+                              // pointText: context.l10n.discounts,
+                              // pointKey: KWidgetkeys.screen.discounts.
+                              // titlePoint,
+                              isDesk: isDesk,
+                              titleSecondPartPadding: const EdgeInsets.only(
+                                left: KPadding.kPaddingSize72,
+                              ),
+                              iconCrossAxisAlignment: CrossAxisAlignment.end,
+                              isRightArrow: false,
+                            ),
+                          ),
+                        ),
+                        SliverPadding(
+                          padding: padding,
+                          sliver: SliverToBoxAdapter(
+                            child: DiscountsFilterWidget(isDesk: isDesk),
+                          ),
+                        ),
+                        SliverPadding(
+                          padding: padding,
+                          sliver: isDesk
+                              ? DiscountsDeskWidgetList(
+                                  maxHeight: constraints.maxHeight,
+                                )
+                              : DiscountsMobWidgetList(),
+                        ),
+                      ],
+                      controller: scrollController,
+                      // semanticChildCount: null,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        );
-      },
+          );
+          return Config.isWeb ? scaffold : SafeArea(child: scaffold);
+        },
+      ),
     );
+
+    // return LayoutBuilder(
+    //   builder: (context, constraints) {
+    //     final isDesk =
+    //         constraints.maxWidth > KPlatformConstants.minWidthThresholdDesk;
+    //     final isTablet =
+    //         constraints.maxWidth > KPlatformConstants.minWidthThresholdTablet;
+    //     final padding = EdgeInsets.symmetric(
+    //       horizontal: (isDesk
+    //           ? KPadding.kPaddingSize90 +
+    //               ((constraints.maxWidth >
+    //                       KPlatformConstants.maxWidthThresholdTablet)
+    //                   ? (constraints.maxWidth -
+    //                           KPlatformConstants.maxWidthThresholdTablet) /
+    //                       2
+    //                   : 0)
+    //           : KPadding.kPaddingSize16),
+    //     );
+    //     final body = [
+    //       NavbarWidget(isDesk: isDesk, isTablet: isTablet),
+    //       KSizedBox.kHeightSizedBox24,
+    //       if (Config.isWeb) ...[
+    //         Padding(
+    //           padding: padding,
+    //           child: TitlePointWidget(
+    //             title: context.l10n.specialOffers,
+    //             titleKey: KWidgetkeys.screen.discounts.title,
+    //             titleSecondPart: context.l10n.forVeteransAndTheirFamilies,
+    //             // pointText: context.l10n.discounts,
+    //             // pointKey: KWidgetkeys.screen.discounts.
+    //             // titlePoint,
+    //             isDesk: isDesk,
+    //             titleSecondPartPadding: const EdgeInsets.only(
+    //               left: KPadding.kPaddingSize72,
+    //             ),
+    //             iconCrossAxisAlignment: CrossAxisAlignment.end,
+    //             isRightArrow: false,
+    //           ),
+    //         ),
+    //         if (isDesk)
+    //           KSizedBox.kHeightSizedBox40
+    //         else
+    //           KSizedBox.kHeightSizedBox24,
+    //       ],
+    //       Padding(
+    //         padding: padding,
+    //         child: DiscountsFilterWidget(isDesk: isDesk),
+    //       ),
+    //       if (isDesk)
+    //         KSizedBox.kHeightSizedBox40
+    //       else
+    //         KSizedBox.kHeightSizedBox24,
+    //       Padding(
+    //         padding: padding,
+    //         child: DiscountsWidgetList(
+    //           isDesk: isDesk,
+    //         ),
+    //       ),
+    //       if (isDesk)
+    //         KSizedBox.kHeightSizedBox40
+    //       else
+    //         KSizedBox.kHeightSizedBox24,
+    //     ];
+    //     return BlocListener<DiscountWatcherBloc, DiscountWatcherState>(
+    //       listener: (context, state) {
+    //         if (state.failure != null) {
+    //           context.dialog.showGetErrorDialog(
+    //             error: state.failure!.value(context),
+    //             onPressed: () {},
+    //             // I think this event is not necessary for Stream, but
+    //             // I think it's better to give
+    //             // the user imaginary control over it
+
+    //             // () => context
+    //             //     .read<DiscountWatcherBloc>()
+    //             //     .add(const DiscountWatcherEvent.started()),
+    //           );
+    //         }
+    //         if (context.read<UserEmailFormBloc>().state.emailEnum.show &&
+    //             state.itemsLoaded ==
+    //                 (context.read<DiscountConfigCubit>().state.loadingItems *
+    //                     (context
+    //                             .read<DiscountConfigCubit>()
+    //                             .state
+    //                             .emailScrollCount +
+    //                         1))) {
+    //           if (Config.isWeb) {
+    //             context.dialog.showUserEmailDialog(
+    //               context.read<DiscountConfigCubit>().state.emailCloseDelay,
+    //             );
+    //           } else {
+    //             context.read<MobileRatingCubit>().showDialog();
+    //           }
+    //         }
+    //       },
+    //       child: BlocListener<NetworkCubit, NetworkStatus>(
+    //         listener: (context, state) {
+    //           if (state == NetworkStatus.network) {
+    //             context.read<DiscountWatcherBloc>().add(
+    //                   DiscountWatcherEvent.started(
+    //                     isEnglish: context.isEnglish,
+    //                   ),
+    //                 );
+    //           }
+    //         },
+    //         child: BlocListener<UrlCubit, UrlEnum?>(
+    //           listener: (context, state) async {
+    //             if (state != null) {
+    //               context.dialog.showSnackBardTextDialog(
+    //                 state.value(context),
+    //                 duration: const Duration(milliseconds: 4000),
+    //               );
+    //               context.read<UrlCubit>().reset();
+    //             }
+    //           },
+    //           child: ListView.builder(
+    //             key: KWidgetkeys.widget.scaffold.scroll,
+    //             // primary: true,
+    //             controller: scrollController,
+    //             restorationId: 'discount_page',
+    //             itemCount: body.length,
+    //             addAutomaticKeepAlives: false,
+    //             addRepaintBoundaries: false,
+    //             itemBuilder: (context, index) => body.elementAt(index),
+    //           ),
+    //         ),
+    //       ),
+    //     );
+    //   },
+    // );
 
     // return BlocBuilder<DiscountConfigCubit, DiscountConfigState>(
     //   builder: (context, config) {
