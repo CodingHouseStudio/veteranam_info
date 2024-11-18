c90cebce8c96ebdf6ed9c962e8f557d5b78a61f1
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-11-18 12:19:39 +0200

    discount screen return email dialog

--- lib/components/discounts/widget/discount_title_widget.dart
+++ lib/components/discounts/widget/discount_title_widget.dart
@@ -4,33 +4,28 @@ import 'package:veteranam/shared/shared_flutter.dart';
 class DiscountTitleWidget extends StatelessWidget {
   const DiscountTitleWidget({
     required this.isDesk,
-    required this.padding,
     super.key,
   });
   final bool isDesk;
-  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
-    return SliverPadding(
-      padding: padding,
-      sliver: SliverToBoxAdapter(
-        child: Padding(
-          padding: const EdgeInsets.only(top: KPadding.kPaddingSize24),
-          child: TitlePointWidget(
-            title: context.l10n.specialOffers,
-            titleKey: KWidgetkeys.screen.discounts.title,
-            titleSecondPart: context.l10n.forVeteransAndTheirFamilies,
-            // pointText: context.l10n.discounts,
-            // pointKey: KWidgetkeys.screen.discounts.
-            // titlePoint,
-            isDesk: isDesk,
-            titleSecondPartPadding: const EdgeInsets.only(
-              left: KPadding.kPaddingSize72,
-            ),
-            iconCrossAxisAlignment: CrossAxisAlignment.end,
-            isRightArrow: false,
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.only(top: KPadding.kPaddingSize24),
+        child: TitlePointWidget(
+          title: context.l10n.specialOffers,
+          titleKey: KWidgetkeys.screen.discounts.title,
+          titleSecondPart: context.l10n.forVeteransAndTheirFamilies,
+          // pointText: context.l10n.discounts,
+          // pointKey: KWidgetkeys.screen.discounts.
+          // titlePoint,
+          isDesk: isDesk,
+          titleSecondPartPadding: const EdgeInsets.only(
+            left: KPadding.kPaddingSize72,
           ),
+          iconCrossAxisAlignment: CrossAxisAlignment.end,
+          isRightArrow: false,
         ),
       ),
     );
