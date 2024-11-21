2a240ce5abfedeffc6e21f173db2b2a80283703c
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-11-21 11:58:02 +0200

    small fix

--- lib/components/discounts/widget/body/discounts_body_widget.dart
+++ lib/components/discounts/widget/body/discounts_body_widget.dart
@@ -48,19 +48,17 @@ class _DiscountsBodyWidget extends StatelessWidget {
             );
           }
           if (state.itemsLoaded ==
-                  (context.read<DiscountConfigCubit>().state.loadingItems *
-                      (context
-                              .read<DiscountConfigCubit>()
-                              .state
-                              .emailScrollCount +
-                          1)) &&
-              context.read<UserEmailFormBloc>().state.emailEnum.show) {
+              (context.read<DiscountConfigCubit>().state.loadingItems *
+                  (context.read<DiscountConfigCubit>().state.emailScrollCount +
+                      1))) {
             if (Config.isWeb) {
-              if ((context.read<UserWatcherBloc>().state.user.email ?? '')
-                  .isEmpty) {
-                context.dialog.showUserEmailDialog(
-                  context.read<DiscountConfigCubit>().state.emailCloseDelay,
-                );
+              if (context.read<UserEmailFormBloc>().state.emailEnum.show) {
+                if ((context.read<UserWatcherBloc>().state.user.email ?? '')
+                    .isEmpty) {
+                  context.dialog.showUserEmailDialog(
+                    context.read<DiscountConfigCubit>().state.emailCloseDelay,
+                  );
+                }
               }
             } else {
               context.read<MobileRatingCubit>().showDialog();
@@ -98,7 +96,7 @@ class _DiscountsBodyWidget extends StatelessWidget {
                     NavigationBarWidget(
                       isDesk: isDesk,
                       isTablet: isTablet,
-                      pageName: context.l10n.discount,
+                      pageName: context.l10n.discounts,
                     ),
                     SliverPadding(
                       padding: padding,
