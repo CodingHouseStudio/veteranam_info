b2aea6fee9797121c1b352343d6314f01b81956e
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-12-02 12:58:42 +0200

    merge

--- lib/components/discounts/bloc/watcher/discount_watcher_bloc.dart
+++ lib/components/discounts/bloc/watcher/discount_watcher_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:collection/collection.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
