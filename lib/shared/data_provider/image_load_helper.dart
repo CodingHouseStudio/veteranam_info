4891f39cb78acf2124fc32e17116bfc0b59214e8
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-12-12 17:42:56 +0200

    revery changes

--- lib/shared/data_provider/image_load_helper.dart
+++ lib/shared/data_provider/image_load_helper.dart
@@ -55,7 +55,7 @@ class ArtifactDownloadHelper {
       // (withCredentials: true);
 
       final response = await _dio.get<Uint8List>(
-        image.downloadURL.getImageUrl,
+        image.downloadURL,
         options: _options,
       );
       if (response.data == null ||
