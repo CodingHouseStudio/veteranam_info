bd7a871f8d54051daebd96dddfc95228c7b269e8
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2024-12-24 12:43:28 +0200

    refactor

--- test/bloc/authentication/authentication_bloc_test.dart
+++ test/bloc/authentication/authentication_bloc_test.dart
@@ -1,9 +1,9 @@
-import 'package:bloc_test/bloc_test.dart';
-import 'package:dartz/dartz.dart';
+// import 'package:bloc_test/bloc_test.dart';
+// import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
-import 'package:mockito/mockito.dart';
-import 'package:veteranam/shared/shared_dart.dart';
+// import 'package:mockito/mockito.dart';
+// import 'package:veteranam/shared/shared_dart.dart';
 
 import '../../test_dependency.dart';
 
