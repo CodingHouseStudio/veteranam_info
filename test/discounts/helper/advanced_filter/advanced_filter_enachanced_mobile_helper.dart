1f3cac663d2ee4ee0ba43908dc303d0fd8ff772b
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-01-24 09:04:24 +0200

    fix tests

--- test/discounts/helper/advanced_filter/advanced_filter_enachanced_mobile_helper.dart
+++ test/discounts/helper/advanced_filter/advanced_filter_enachanced_mobile_helper.dart
@@ -132,6 +132,33 @@ Future<void> advancedFilterEnchancedMobileHelper(
     tester: tester,
     offset: KTestConstants.scrollingDown,
     scrollKey: DiscountsFilterKeys.list,
+    itemKey: DiscountsFilterKeys.citiesText,
+  );
+
+  for (var i = 0; i < 2; i++) {
+    if (find.byKey(DiscountsFilterKeys.cityItems).evaluate().isEmpty) {
+      await tester.tap(find.byKey(DiscountsFilterKeys.citiesText));
+    }
+  }
+
+  await scrollingHelper(
+    tester: tester,
+    offset: KTestConstants.scrollingDown,
+    scrollKey: DiscountsFilterKeys.list,
+    itemKey: DiscountsFilterKeys.citySearchField,
+  );
+
+  await tester.enterText(
+    find.byKey(DiscountsFilterKeys.citySearchField),
+    KTestVariables.fieldEmpty,
+  );
+
+  await tester.pumpAndSettle();
+
+  await scrollingHelper(
+    tester: tester,
+    offset: KTestConstants.scrollingDown,
+    scrollKey: DiscountsFilterKeys.list,
     itemKey: DiscountsFilterKeys.citySearchField,
   );
 
