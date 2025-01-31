8b800033809ff08f79d082e9b84b5cf30851d494
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-01-31 11:49:56 +0200

    remove unused app layout module

--- lib/shared/repositories/app_layout_module.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:injectable/injectable.dart';
-
-@module
-abstract class AppLayoutModule {
-  @singleton
-  WidgetsBinding get widgetsBinding => WidgetsBinding.instance;
-}
