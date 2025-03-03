8c88f5e5ab339c01af5738e9d2ce27042c9499e4
DemianKryk <158501490+DemianKryk@users.noreply.github.com>
2025-03-03 10:06:00 +0200

    feat: Popup menu widget user custom package (#1015)
    popup menu widget use custom package

--- lib/shared/constants/models_flutter.dart
+++ lib/shared/constants/models_flutter.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/widgets.dart' show Key, VoidCallback, Widget;
+// import 'package:flutter/widgets.dart' show Key, VoidCallback, Widget;
 
-class PopupMenuCustomItem<T> {
-  PopupMenuCustomItem({
-    required this.value,
-    required this.text,
-    required this.event,
-    required this.key,
-    this.enabled = true,
-    this.icon,
-  });
+// class CustomDropDownButtonItem<T> {
+//   CustomDropDownButtonItem({
+//     required this.value,
+//     required this.text,
+//     required this.event,
+//     required this.key,
+//     this.enabled = true,
+//     this.icon,
+//   });
 
-  final String text;
-  final VoidCallback? event;
-  final Key? key;
-  final bool enabled;
-  final T value;
-  final Widget? icon;
-}
+//   final String text;
+//   final VoidCallback? event;
+//   final Key? key;
+//   final bool enabled;
+//   final T value;
+//   final Widget? icon;
+// }
